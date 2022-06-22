e5e3a08dba7e6fad5784251c53e9e92771a6c805
Viktor <viktor.grondalen.solberg@nav.no>
2022-06-22 17:37:51 +0200

    skal kunne sende test-tekst til frontend fra testController

---
+++ src/main/kotlin/no/nav/familie/klage/test/TestController.kt
@@ -0,0 +1,23 @@
+package no.nav.familie.klage.test
+
+import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.MediaType
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(path = ["/api/test"], produces = [MediaType.APPLICATION_JSON_VALUE])
+@ProtectedWithClaims(issuer = "azuread")
+@Validated
+class TestController {
+
+    @GetMapping
+    fun hentSøknadDatoer(): Ressurs<String> {
+        return Ressurs.success(
+                "Hentet tekst fra backend"
+        )
+    }
+}
