fc2357ff574814250719236a0091f3ae3bee818d
junileirvik <juni.leirvik@gmail.com>
2022-07-04 12:44:26 +0200

    fjernet å og ø og fjernet noen variabler fra vurdering, som må legges til i formkrav senere.

--- src/main/kotlin/no/nav/familie/klage/vurdering/domain/Vurdering.kt
+++ src/main/kotlin/no/nav/familie/klage/vurdering/domain/Vurdering.kt
@@ -1,26 +1,20 @@
 package no.nav.familie.klage.vurdering.domain
 
 import org.springframework.data.annotation.Id
-import org.springframework.data.relational.core.mapping.Column
 import java.time.LocalDateTime
 import java.util.UUID
 
 data class Vurdering(
     @Id
     val behandlingId: UUID = UUID.randomUUID(),
-    val oppfyltFormkrav: Int,
-    val muligFormkrav: Int,
-    val begrunnelse: String,
-    val vedtakValg: VedtakValg,
-    @Column("årsak")
-    val årsak: Årsak? = null,
+    val vedtak: Vedtak,
+    val arsak: Årsak? = null,
     val hjemmel: Hjemmel? = null,
     val beskrivelse: String,
-    @Column("fullført_dato")
-    val fullførtDato: LocalDateTime? = null
+    val fullfortDato: LocalDateTime? = LocalDateTime.now()
 )
 
-enum class VedtakValg {
+enum class Vedtak {
     OMGJØR_VEDTAK,
     OPPRETTHOLD_VEDTAK,
 }
--- src/main/resources/db/migration/V5__vurdering.sql
+++ src/main/resources/db/migration/V5__vurdering.sql
@@ -1,13 +1,8 @@
 CREATE TABLE vurdering (
-    behandling_id        UUID PRIMARY KEY REFERENCES behandling (id),
-
-    oppfylt_formkrav     INT NOT NULL,
-    mulig_formkrav       INT NOT NULL,
-    begrunnelse          VARCHAR NOT NULL,
-
-    vedtak_valg          VARCHAR NOT NULL,
-    årsak                VARCHAR,
+    behandling_id        UUID PRIMARY KEY,
+    vedtak               VARCHAR NOT NULL,
+    arsak                VARCHAR,
     hjemmel              VARCHAR,
     beskrivelse          VARCHAR NOT NULL,
-    fullført_dato       TIMESTAMP DEFAULT LOCALTIMESTAMP
+    fullfort_dato        TIMESTAMP DEFAULT LOCALTIMESTAMP
 );
