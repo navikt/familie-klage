96d750894ccae24c85cebc5a352c6573ed645fe1
maja <maja_ying@hotmail.com>
2022-07-04 14:30:04 +0200

    fikset pr-kommentarer

--- src/main/kotlin/no/nav/familie/klage/formkrav/FormController.kt
+++ src/main/kotlin/no/nav/familie/klage/formkrav/FormController.kt
@@ -2,7 +2,6 @@ package no.nav.familie.klage.formkrav
 
 import no.nav.familie.klage.formkrav.domain.Form
 import no.nav.familie.klage.formkrav.dto.FormDto
-import no.nav.familie.klage.formkrav.dto.tilDto
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.validation.annotation.Validated
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
 
 @RestController
 @RequestMapping(path = ["/api/formkrav"])
@@ -24,13 +22,11 @@ class FormController(
 
     @GetMapping("{formId}")
     fun hentForm(@PathVariable formId: String): Ressurs<FormDto> {
-        println("==> Oppretter formDto i hentForm")
         return Ressurs.success(formService.opprettFormDto())
     }
 
     @PostMapping
     fun opprettFormkravVilkår(@RequestBody form: Form): Ressurs<Form> {
-        println(form)
         return Ressurs.success(formService.opprettForm(form))
     }
 }
