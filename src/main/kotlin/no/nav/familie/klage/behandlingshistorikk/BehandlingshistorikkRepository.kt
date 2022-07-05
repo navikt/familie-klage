8c026666c035cd1c5469a5143c4c2e7207b85aee
junileirvik <juni.leirvik@gmail.com>
2022-07-05 15:50:07 +0200

    Behandlingshistorikk kan opprettes og status "Opprettet" bilr satt når behandlnig opprettes

---
+++ src/main/kotlin/no/nav/familie/klage/behandlingshistorikk/BehandlingshistorikkRepository.kt
@@ -0,0 +1,14 @@
+package no.nav.familie.klage.behandlingshistorikk
+
+import no.nav.familie.klage.behandlingshistorikk.domain.Behandlingshistorikk
+import no.nav.familie.klage.repository.InsertUpdateRepository
+import no.nav.familie.klage.repository.RepositoryInterface
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface BehandlingshistorikkRepository : RepositoryInterface<Behandlingshistorikk, UUID>, InsertUpdateRepository<Behandlingshistorikk> {
+
+    fun findByBehandlingId(behandlingId: UUID): List<Behandlingshistorikk>
+
+}
