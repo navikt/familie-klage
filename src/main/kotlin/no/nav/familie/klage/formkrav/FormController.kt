298a76d25cc429515ec12ed070f15eb2ab873b94
maja <maja_ying@hotmail.com>
2022-08-04 13:51:27 +0200

    validerer saksbehandlertilgang ved opprettelse og henting av formkrav

--- src/main/kotlin/no/nav/familie/klage/formkrav/FormController.kt
+++ src/main/kotlin/no/nav/familie/klage/formkrav/FormController.kt
@@ -1,7 +1,9 @@
 package no.nav.familie.klage.formkrav
 
+import no.nav.familie.klage.felles.domain.AuditLoggerEvent
 import no.nav.familie.klage.formkrav.domain.Form
 import no.nav.familie.klage.formkrav.dto.FormDto
+import no.nav.familie.klage.infrastruktur.sikkerhet.TilgangService
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.validation.annotation.Validated
@@ -18,16 +20,20 @@ import java.util.UUID
 @ProtectedWithClaims(issuer = "azuread")
 @Validated
 class FormController(
-    private val formService: FormService
+    private val formService: FormService,
+    private val tilgangService: TilgangService,
 ) {
 
     @GetMapping("vilkar/{behandlingId}")
     fun hentVilkår(@PathVariable behandlingId: UUID): Ressurs<FormDto?> {
+        tilgangService.validerTilgangTilBehandling(behandlingId, AuditLoggerEvent.ACCESS)
         return Ressurs.success(formService.hentForm(behandlingId))
     }
 
     @PostMapping
     fun opprettFormkravVilkår(@RequestBody form: Form): Ressurs<FormDto> {
+        tilgangService.validerTilgangTilBehandling(form.behandlingId, AuditLoggerEvent.UPDATE)
+        tilgangService.validerHarSaksbehandlerrolle()
         return Ressurs.success(formService.opprettForm(form))
     }
 }
