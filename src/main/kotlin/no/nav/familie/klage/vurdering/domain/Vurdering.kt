e4c4600a912418ca1ebc09a40d0536dbae5801c0
Viktor <viktor.grondalen.solberg@nav.no>
2022-09-12 09:12:37 +0200

    legger til hjemler og årsaker fra kabal

--- src/main/kotlin/no/nav/familie/klage/vurdering/domain/Vurdering.kt
+++ src/main/kotlin/no/nav/familie/klage/vurdering/domain/Vurdering.kt
@@ -6,13 +6,13 @@ import org.springframework.data.relational.core.mapping.Embedded
 import java.util.UUID
 
 data class Vurdering(
-    @Id
+        @Id
     val behandlingId: UUID,
-    val vedtak: Vedtak,
-    val arsak: Arsak? = null,
-    val hjemmel: Hjemmel? = null,
-    val beskrivelse: String,
-    @Embedded(onEmpty = Embedded.OnEmpty.USE_EMPTY)
+        val vedtak: Vedtak,
+        val arsak: Årsak? = null,
+        val hjemmel: Hjemmel? = null,
+        val beskrivelse: String,
+        @Embedded(onEmpty = Embedded.OnEmpty.USE_EMPTY)
     val sporbar: Sporbar = Sporbar()
 )
 
@@ -22,12 +22,14 @@ enum class Vedtak {
     OPPRETTHOLD_VEDTAK,
 }
 
-enum class Arsak {
+enum class Årsak {
     VELG,
-    SAKSBEHANDLINGSFEIL,
-    TRYKKET_FEIL,
-    LESTE_IKKE_GJENNOM,
-    ENDRET_REGELVERK,
+    FEIL_I_LOVANDVENDELSE,
+    FEIL_REGELVERKSFORSTÅELSE,
+    FEIL_ELLER_ENDRET_FAKTA,
+    FEIL_PROSESSUELL,
+    KØET_BEHANDLING,
+    ANNET
 }
 
 enum class Hjemmel {
@@ -35,4 +37,18 @@ enum class Hjemmel {
     FEMTEN_TO,
     FEMTEN_TRE,
     FEMTEN_FIRE,
+    FEMTEN_FEM,
+    FEMTEN_SEKS,
+    FEMTEN_ÅTTE,
+    FEMTEN_NI,
+    FEMTEN_TI,
+    FEMTEN_ELLEVE,
+    FEMTEN_TOLV,
+    FEMTEN_TRETTEN,
+    TO,
+    FIRE,
+    FEM,
+    NI,
+    TRETTEN,
+    EØS
 }
