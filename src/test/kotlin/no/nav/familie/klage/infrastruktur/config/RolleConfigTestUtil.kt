e5096b945016551aa72d738da1f041443964ed0c
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-09-22 13:42:51 +0200

    Tester på tilgang på fagsystem og roller

--- src/test/kotlin/no/nav/familie/klage/infrastruktur/config/RolleConfigTestUtil.kt
+++ src/test/kotlin/no/nav/familie/klage/infrastruktur/config/RolleConfigTestUtil.kt
@@ -3,8 +3,8 @@ package no.nav.familie.klage.infrastruktur.config
 object RolleConfigTestUtil {
 
     val rolleConfig = RolleConfig(
-        ba = FagsystemRolleConfig("", "", ""),
-        ef = FagsystemRolleConfig("", "", "efVeileder"),
-        ks = FagsystemRolleConfig("", "", "")
+        ba = FagsystemRolleConfig("baSaksbehandler", "baBeslutter", "baVeileder"),
+        ef = FagsystemRolleConfig("efSaksbehandler", "efBeslutter", "efVeileder"),
+        ks = FagsystemRolleConfig("ksSaksbehandler", "ksBeslutter", "ksVeileder")
     )
 }
--- src/test/kotlin/no/nav/familie/klage/infrastruktur/sikkerhet/TilgangServiceTest.kt
+++ src/test/kotlin/no/nav/familie/klage/infrastruktur/sikkerhet/TilgangServiceTest.kt
@@ -3,7 +3,9 @@ package no.nav.familie.klage.infrastruktur.sikkerhet
 import io.mockk.every
 import io.mockk.mockk
 import no.nav.familie.klage.behandling.BehandlingService
+import no.nav.familie.klage.behandling.domain.Behandling
 import no.nav.familie.klage.fagsak.FagsakService
+import no.nav.familie.klage.fagsak.domain.Fagsak
 import no.nav.familie.klage.felles.domain.AuditLogger
 import no.nav.familie.klage.felles.domain.BehandlerRolle
 import no.nav.familie.klage.infrastruktur.config.RolleConfigTestUtil
@@ -11,6 +13,8 @@ import no.nav.familie.klage.personopplysninger.PersonopplysningerIntegrasjonerCl
 import no.nav.familie.klage.testutil.BrukerContextUtil.testWithBrukerContext
 import no.nav.familie.klage.testutil.DomainUtil.behandling
 import no.nav.familie.klage.testutil.DomainUtil.fagsak
+import no.nav.familie.kontrakter.felles.klage.Stønadstype
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -34,11 +38,18 @@ internal class TilgangServiceTest {
         fagsakService
     )
 
-    private val fagsak = fagsak()
-    private val behandling = behandling(fagsak)
+    private val fagsakEf = fagsak()
+    private val behandlingEf = behandling(fagsakEf)
+    private val fagsakBa = fagsak(stønadstype = Stønadstype.BARNETRYGD)
+    private val behandlingBa = behandling(fagsakBa)
 
     @BeforeEach
     internal fun setUp() {
+        mockFagsakOgBehandling(fagsakEf, behandlingEf)
+        mockFagsakOgBehandling(fagsakBa, behandlingBa)
+    }
+
+    private fun mockFagsakOgBehandling(fagsak: Fagsak, behandling: Behandling) {
         every { fagsakService.hentFagsak(fagsak.id) } returns fagsak
         every { behandlingService.hentBehandling(behandling.id) } returns behandling
     }
@@ -47,10 +58,45 @@ internal class TilgangServiceTest {
     inner class TilgangGittRolle {
 
         @Test
-        internal fun `saksbehandler har tilgang til behandling av fagsystem enslig forsørger`() {
-            testWithBrukerContext(groups = listOf(rolleConfig.ef.veileder)) {
-                tilgangService.harTilgangTilBehandlingGittRolle(behandling.id, BehandlerRolle.VEILEDER)
+        internal fun `saksbehandler har tilgang til behandling av fagsystem barnetrygd`() {
+            testWithBrukerContext(groups = listOf(rolleConfig.ba.saksbehandler)) {
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingBa.id, BehandlerRolle.SAKSBEHANDLER)).isTrue
             }
         }
+
+        @Test
+        internal fun `ef-saksbehandler har ikke tilgang til behandling av fagsystem barnetrygd`() {
+            testWithBrukerContext(groups = listOf(rolleConfig.ef.saksbehandler)) {
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingBa.id, BehandlerRolle.SAKSBEHANDLER)).isFalse
+            }
+        }
+
+        @Test
+        internal fun `veileder har ikke tilgang som saksbehandler eller beslutter`() {
+            testWithBrukerContext(groups = listOf(rolleConfig.ba.veileder)) {
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingBa.id, BehandlerRolle.VEILEDER)).isTrue
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingBa.id, BehandlerRolle.SAKSBEHANDLER)).isFalse
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingBa.id, BehandlerRolle.BESLUTTER)).isFalse
+            }
+        }
+
+        @Test
+        internal fun `saksbehandler har tilgang som veileder og saksbehandler, men ikke beslutter`() {
+            testWithBrukerContext(groups = listOf(rolleConfig.ef.saksbehandler)) {
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingEf.id, BehandlerRolle.VEILEDER)).isTrue
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingEf.id, BehandlerRolle.SAKSBEHANDLER)).isTrue
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingEf.id, BehandlerRolle.BESLUTTER)).isFalse
+            }
+        }
+
+        @Test
+        internal fun `beslutter har tilgang som saksbehandler, beslutter og veileder`() {
+            testWithBrukerContext(groups = listOf(rolleConfig.ef.beslutter)) {
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingEf.id, BehandlerRolle.VEILEDER)).isTrue
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingEf.id, BehandlerRolle.SAKSBEHANDLER)).isTrue
+                assertThat(tilgangService.harTilgangTilBehandlingGittRolle(behandlingEf.id, BehandlerRolle.BESLUTTER)).isTrue
+            }
+        }
+
     }
 }
