bf4794de6a38a3b5ae1df2b28f7bd6dc91104097
Viktor <viktor.grondalen.solberg@nav.no>
2022-09-27 09:59:23 +0200

    skal ikke kunne opprette klage med krav mottatt frem i tid

--- src/main/kotlin/no/nav/familie/klage/behandling/OpprettBehandlingService.kt
+++ src/main/kotlin/no/nav/familie/klage/behandling/OpprettBehandlingService.kt
@@ -3,9 +3,11 @@ package no.nav.familie.klage.behandling
 import no.nav.familie.klage.behandling.domain.Behandling
 import no.nav.familie.klage.fagsak.FagsakService
 import no.nav.familie.klage.formkrav.FormService
+import no.nav.familie.klage.infrastruktur.exception.feilHvis
 import no.nav.familie.kontrakter.felles.klage.OpprettKlagebehandlingRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
 import java.util.UUID
 
 @Service
@@ -19,6 +21,10 @@ class OpprettBehandlingService(
     fun opprettBehandling(
         opprettKlagebehandlingRequest: OpprettKlagebehandlingRequest
     ): UUID {
+        feilHvis(opprettKlagebehandlingRequest.klageMottatt.isAfter(LocalDate.now())) {
+            "Kan ikke opprette klage med krav mottatt frem i tid for behandling med eksternBehandlingId=${opprettKlagebehandlingRequest.eksternBehandlingId}"
+        }
+
         val fagsak = fagsakService.hentEllerOpprettFagsak(
             ident = opprettKlagebehandlingRequest.ident,
             eksternId = opprettKlagebehandlingRequest.eksternFagsakId,
