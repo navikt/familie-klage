39b9dffd122677642f24d1e0eb4d546ca6af109c
Viktor <viktor.grondalen.solberg@nav.no>
2022-10-31 13:58:55 +0100

    skal ikke kunne velge -skal ikke tas stilling til- på formkravsiden

---
+++ src/main/resources/db/migration/V9__slett_skal_ikke_vurderes.sql
@@ -0,0 +1,4 @@
+UPDATE form SET klage_part='IKKE_OPPFYLT' WHERE klage_part='SKAL_IKKE_VURDERES';
+UPDATE form SET klage_konkret='IKKE_OPPFYLT' WHERE klage_konkret='SKAL_IKKE_VURDERES';
+UPDATE form SET klage_signert='IKKE_OPPFYLT' WHERE klage_signert='SKAL_IKKE_VURDERES';
+UPDATE form SET klagefrist_overholdt='IKKE_OPPFYLT' WHERE klagefrist_overholdt='SKAL_IKKE_VURDERES';
