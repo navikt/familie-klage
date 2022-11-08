b95b562ec457f8a48c2c7bb0a8cc69bd348a4f7d
Johan Blomgren <blommish@users.noreply.github.com>
2022-11-08 10:09:45 +0100

    Prometheusmetrikker for behandlinger (#167)
    * Prometheusmetrikker for behandlinger
    
    * Trenger ikke fordelingen per uke når vi registrerer disse metrikkene fra før appen har blitt tatt i bruk

---
+++ src/main/kotlin/no/nav/familie/klage/metrics/domain/AntallVedtak.kt
@@ -0,0 +1,10 @@
+package no.nav.familie.klage.metrics.domain
+
+import no.nav.familie.kontrakter.felles.klage.BehandlingResultat
+import no.nav.familie.kontrakter.felles.klage.Stønadstype
+
+data class AntallVedtak(
+    val stonadstype: Stønadstype,
+    val resultat: BehandlingResultat,
+    val antall: Int
+)
---
+++ src/main/kotlin/no/nav/familie/klage/metrics/domain/BehandlingerPerStatus.kt
@@ -0,0 +1,11 @@
+package no.nav.familie.klage.metrics.domain
+
+import no.nav.familie.kontrakter.felles.klage.BehandlingStatus
+import no.nav.familie.kontrakter.felles.klage.Stønadstype
+
+
+data class BehandlingerPerStatus(
+    val stonadstype: Stønadstype,
+    val status: BehandlingStatus,
+    val antall: Int
+)
