f0eb43356bad2ebbd4054d5d20591f1dffc1f681
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-11-17 12:56:32 +0100

    For alle behandlinger med behandlingsresultat IKKE_MEDHOLD så skal det opprettes et nytt behandlingsstatistikk innslag av type SENDT_TIL_KA, siden vi ikke bryr oss om duplikate innslag.

--- src/main/kotlin/no/nav/familie/klage/behandlingsstatistikk/PatchBehandlingsstatistikkController.kt
+++ src/main/kotlin/no/nav/familie/klage/behandlingsstatistikk/PatchBehandlingsstatistikkController.kt
@@ -1,72 +1,35 @@
 package no.nav.familie.klage.behandlingsstatistikk
 
-import no.nav.familie.klage.behandling.BehandlingService
+import no.nav.familie.klage.behandling.BehandlingRepository
 import no.nav.familie.kontrakter.felles.klage.BehandlingResultat
-import no.nav.familie.prosessering.domene.Status
-import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
 import no.nav.security.token.support.core.api.Unprotected
 import org.slf4j.LoggerFactory
-import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
-import java.util.UUID
 
 @RestController
 @RequestMapping("/api/patch-statistikk")
 @Unprotected
 class PatchBehandlingsstatistikkController(
-    private val behandlingService: BehandlingService,
+    private val behandlingRepository: BehandlingRepository,
     private val taskRepository: TaskRepository
 ) {
-    private val triggerTid = LocalDate.of(2022, 11, 11)
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @PostMapping("/")
     fun patchTidligereBehandlingResultater() {
-        val tasker = hentOgFiltrerTaskerPåTriggertid()
-        tasker.filter { it.erFerdig() }.forEach { task ->
-            val behandlingId = task.getProperty("behandlingId")
-            val behandling = behandlingService.hentBehandling(UUID.fromString(behandlingId))
+        val behandlinger = behandlingRepository.findAll()
+        behandlinger.forEach { behandling ->
             if (behandling.resultat == BehandlingResultat.IKKE_MEDHOLD) {
-                if (sendtTilKATaskErOpprettet(behandlingId, tasker)) {
-                    logger.info("Det finnes allerede en task med behandlingId:$behandlingId som har status FERDIG av typen SENDT_TIL_KA")
-                } else {
-                    logger.info("Oppretter task for behandlingsstatistikk med hendelse SENDT_TIL_KA for behandlingId:${behandling.id} som har behandlingsresultat IKKE_MEDHOLD")
-                    taskRepository.save(
-                        BehandlingsstatistikkTask.opprettSendtTilKATask(
-                            behandlingId = behandling.id,
-                            hendelseTidspunkt = task.opprettetTid
-                        )
-                    )
-                }
+                logger.info("Oppretter task for behandlingsstatistikk med hendelse SENDT_TIL_KA for behandlingId:${behandling.id} som har behandlingsresultat IKKE_MEDHOLD")
+                val taskSomSkalOpprettes = BehandlingsstatistikkTask.opprettSendtTilKATask(
+                    behandlingId = behandling.id,
+                    hendelseTidspunkt = behandling.sporbar.endret.endretTid,
+                )
+                taskRepository.save(taskSomSkalOpprettes)
             }
         }
-    }
-
-    private fun sendtTilKATaskErOpprettet(behandlingId: String, tasker: List<Task>): Boolean {
-        return tasker.any { task ->
-            task.getProperty("behandlingId") == behandlingId && task.getProperty("hendelse") == BehandlingsstatistikkHendelse.SENDT_TIL_KA.name
-        }
-    }
-
-    private fun hentOgFiltrerTaskerPåTriggertid(): List<Task> {
-        return taskRepository.findByStatusInAndType(
-            listOf(Status.MANUELL_OPPFØLGING),
-            BehandlingsstatistikkTask.TYPE,
-            Pageable.unpaged()
-        ).filter { task ->
-            task.triggerTid.toLocalDate() <= triggerTid
-        }
-    }
-
-    private fun Task.getProperty(property: String): String {
-        return this.metadataWrapper.properties.get(property).toString()
-    }
-
-    private fun Task.erFerdig(): Boolean {
-        return this.getProperty("hendelse") == BehandlingsstatistikkHendelse.FERDIG.name
     }
 }
