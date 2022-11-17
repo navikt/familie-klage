690082b276c40b14ecd8c559a288363d7ee1505f
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-11-17 13:07:24 +0100

    Legger på dryrun i PatchBehandlingsstatistikkController for evt testing

--- src/main/kotlin/no/nav/familie/klage/behandlingsstatistikk/PatchBehandlingsstatistikkController.kt
+++ src/main/kotlin/no/nav/familie/klage/behandlingsstatistikk/PatchBehandlingsstatistikkController.kt
@@ -7,7 +7,9 @@ import no.nav.security.token.support.core.api.Unprotected
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.websocket.server.PathParam
 
 @RestController
 @RequestMapping("/api/patch-statistikk")
@@ -18,17 +20,19 @@ class PatchBehandlingsstatistikkController(
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    @PostMapping("/")
-    fun patchTidligereBehandlingResultater() {
+    @PostMapping("/{dryrun}")
+    fun patchTidligereBehandlingResultater(@RequestParam dryrun: Boolean) {
         val behandlinger = behandlingRepository.findAll()
         behandlinger.forEach { behandling ->
             if (behandling.resultat == BehandlingResultat.IKKE_MEDHOLD) {
-                logger.info("Oppretter task for behandlingsstatistikk med hendelse SENDT_TIL_KA for behandlingId:${behandling.id} som har behandlingsresultat IKKE_MEDHOLD")
-                val taskSomSkalOpprettes = BehandlingsstatistikkTask.opprettSendtTilKATask(
-                    behandlingId = behandling.id,
-                    hendelseTidspunkt = behandling.sporbar.endret.endretTid,
-                )
-                taskRepository.save(taskSomSkalOpprettes)
+                logger.info("Oppretter task for behandlingsstatistikk med hendelse SENDT_TIL_KA for behandlingId:${behandling.id} som har behandlingsresultat ${behandling.resultat.name}. Dryrun : $dryrun")
+                if (!dryrun) {
+                    val taskSomSkalOpprettes = BehandlingsstatistikkTask.opprettSendtTilKATask(
+                        behandlingId = behandling.id,
+                        hendelseTidspunkt = behandling.sporbar.endret.endretTid,
+                    )
+                    taskRepository.save(taskSomSkalOpprettes)
+                }
             }
         }
     }
