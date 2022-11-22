00e95430da81193ecb12c49d54efb8537b110529
Johan Blomgren <blommish@users.noreply.github.com>
2022-11-22 09:20:15 +0100

    Brevsignatur skal anonymisere saksbehandlers navn og enhet hvis strengt fortrolig (#180)

--- src/main/kotlin/no/nav/familie/klage/brev/BrevsignaturService.kt
+++ src/main/kotlin/no/nav/familie/klage/brev/BrevsignaturService.kt
@@ -2,17 +2,24 @@ package no.nav.familie.klage.brev
 
 import no.nav.familie.klage.brev.dto.SignaturDto
 import no.nav.familie.klage.infrastruktur.sikkerhet.SikkerhetContext
+import no.nav.familie.klage.personopplysninger.dto.PersonopplysningerDto
 import org.springframework.stereotype.Service
-import java.util.UUID
 
 @Service
-class BrevsignaturService() {
+class BrevsignaturService {
 
-    fun lagSignatur(behandlingId: UUID): SignaturDto {
-        return SignaturDto(SikkerhetContext.hentSaksbehandlerNavn(true), ENHET_NAY, false)
+    fun lagSignatur(personopplysningerDto: PersonopplysningerDto): SignaturDto {
+        val harStrengtFortroligAdresse: Boolean = personopplysningerDto.adressebeskyttelse?.erStrengtFortrolig() ?: false
+
+        return if (harStrengtFortroligAdresse) {
+            SignaturDto(NAV_ANONYM_NAVN, ENHET_VIKAFOSSEN)
+        } else {
+            SignaturDto(SikkerhetContext.hentSaksbehandlerNavn(true), ENHET_NAY)
+        }
     }
 
     companion object {
+
         val NAV_ANONYM_NAVN = "NAV anonym"
         val ENHET_VIKAFOSSEN = "NAV Vikafossen"
         val ENHET_NAY = "NAV Arbeid og ytelser"
---
+++ src/test/kotlin/no/nav/familie/klage/brev/BrevsignaturServiceTest.kt
@@ -0,0 +1,39 @@
+package no.nav.familie.klage.brev
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.klage.personopplysninger.dto.Adressebeskyttelse
+import no.nav.familie.klage.personopplysninger.dto.PersonopplysningerDto
+import no.nav.familie.klage.testutil.BrukerContextUtil.testWithBrukerContext
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class BrevsignaturServiceTest {
+
+    val brevsignaturService = BrevsignaturService()
+
+    @Test
+    fun `skal anonymisere signatur hvis strengt fortrolig`() {
+        val personopplysningerDto = mockk<PersonopplysningerDto>()
+        every { personopplysningerDto.adressebeskyttelse } returns Adressebeskyttelse.STRENGT_FORTROLIG
+
+        val signaturMedEnhet = brevsignaturService.lagSignatur(personopplysningerDto)
+
+        assertThat(signaturMedEnhet.enhet).isEqualTo(BrevsignaturService.ENHET_VIKAFOSSEN)
+        assertThat(signaturMedEnhet.navn).isEqualTo(BrevsignaturService.NAV_ANONYM_NAVN)
+    }
+
+    @Test
+    internal fun `skal returnere saksehandlers navn og enhet hvis ikke strengt fortrolig`() {
+        val personopplysningerDto = mockk<PersonopplysningerDto>()
+        every { personopplysningerDto.adressebeskyttelse } returns null
+
+        val signaturMedEnhet = testWithBrukerContext(preferredUsername = "Julenissen") {
+            brevsignaturService.lagSignatur(personopplysningerDto)
+        }
+
+        assertThat(signaturMedEnhet.enhet).isEqualTo(BrevsignaturService.ENHET_NAY)
+        assertThat(signaturMedEnhet.navn).isEqualTo("Julenissen")
+    }
+
+}
