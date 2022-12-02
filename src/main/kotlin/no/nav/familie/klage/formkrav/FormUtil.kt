058d60710bfd5f3b0f64f9dfb5fe977e54ba2006
Johan Blomgren <blommish@users.noreply.github.com>
2022-12-02 14:02:31 +0100

    Fortydliget flyt og validering av av formkrav

--- src/main/kotlin/no/nav/familie/klage/formkrav/FormUtil.kt
+++ src/main/kotlin/no/nav/familie/klage/formkrav/FormUtil.kt
@@ -6,23 +6,30 @@ import no.nav.familie.klage.formkrav.domain.FormVilkår
 
 object FormUtil {
 
-    fun ferdigUtfylt(formkrav: Form, påklagetVedtak: PåklagetVedtakDto) =
-        påklagetVedtak.harTattStillingTil() &&
-            alleVilkårBesvart(formkrav) &&
-            (alleVilkårOppfylt(formkrav) || friteksterUtfylt(formkrav))
+    fun formresultat(formkrav: Form, påklagetVedtak: PåklagetVedtakDto): FormVilkår {
+
+        return when {
+            !påklagetVedtak.harTattStillingTil() -> FormVilkår.IKKE_SATT
+            !alleVilkårBesvart(formkrav) -> FormVilkår.IKKE_SATT
+            alleVilkårOppfylt(formkrav) -> FormVilkår.OPPFYLT
+            friteksterUtfylt(formkrav) -> FormVilkår.IKKE_OPPFYLT
+            else -> FormVilkår.IKKE_SATT
+        }
+    }
 
     fun alleVilkårOppfylt(formkrav: Form): Boolean {
         return formkrav.alleSvar().all { it == FormVilkår.OPPFYLT }
     }
 
-    fun alleVilkårBesvart(formkrav: Form): Boolean {
+    private fun alleVilkårBesvart(formkrav: Form): Boolean {
         return formkrav.alleSvar().none { it == FormVilkår.IKKE_SATT }
     }
 
     fun friteksterUtfylt(formkrav: Form) = formkrav.saksbehandlerBegrunnelse != null &&
-        formkrav.saksbehandlerBegrunnelse.isNotBlank() &&
-        formkrav.brevtekst != null &&
-        formkrav.brevtekst.isNotBlank()
+            formkrav.saksbehandlerBegrunnelse.isNotBlank() &&
+            formkrav.brevtekst != null &&
+            formkrav.brevtekst.isNotBlank()
+
     private fun Form.alleSvar() = setOf(
         klageKonkret,
         klagePart,
