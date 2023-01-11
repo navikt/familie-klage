45ff0f795f59d326f0c0fba49344f852f94a01f5
halvorbmundal <halvorbmundal@users.noreply.github.com>
2023-01-11 12:11:35 +0100

    Update src/main/kotlin/no/nav/familie/klage/behandling/domain/Behandling.kt
    Co-authored-by: Johan Blomgren <blommish@users.noreply.github.com>

--- src/main/kotlin/no/nav/familie/klage/behandling/domain/Behandling.kt
+++ src/main/kotlin/no/nav/familie/klage/behandling/domain/Behandling.kt
@@ -40,7 +40,7 @@ data class PåklagetVedtakDetaljer(
     val behandlingstype: String,
     val resultat: String,
     val vedtakstidspunkt: LocalDateTime,
-    val regelverk: Regelverk? = Regelverk.NASJONAL
+    val regelverk: Regelverk?
 )
 
 data class PåklagetVedtak(
