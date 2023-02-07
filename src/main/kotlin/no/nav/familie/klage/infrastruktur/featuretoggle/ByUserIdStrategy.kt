f17c2f7fe8487d7c8a719c84c79d90bcbeceed81
ma10s <mattis.janitz@nav.no>
2023-02-07 12:30:10 +0100

    Ny unleash client (io.getunleash) - vil bruke en client som fortsatt oppdateres.

--- src/main/kotlin/no/nav/familie/klage/infrastruktur/featuretoggle/ByUserIdStrategy.kt
+++ src/main/kotlin/no/nav/familie/klage/infrastruktur/featuretoggle/ByUserIdStrategy.kt
@@ -1,6 +1,6 @@
 package no.nav.familie.klage.infrastruktur.featuretoggle
 
-import no.finn.unleash.strategy.Strategy
+import io.getunleash.strategy.Strategy
 import no.nav.familie.klage.infrastruktur.sikkerhet.SikkerhetContext
 
 class ByUserIdStrategy : Strategy {
