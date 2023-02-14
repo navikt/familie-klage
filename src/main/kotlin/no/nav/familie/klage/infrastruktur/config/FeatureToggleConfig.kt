d8ecfce5d22116f68abb96034feb1713bedaaae7
Johan Blomgren <blommish@users.noreply.github.com>
2023-02-14 10:57:13 +0100

    Fjerner @ConstructorBinding som nå er unødvendig sammen med @ConfigurationProperties

--- src/main/kotlin/no/nav/familie/klage/infrastruktur/config/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/klage/infrastruktur/config/FeatureToggleConfig.kt
@@ -11,18 +11,15 @@ import no.nav.familie.klage.infrastruktur.featuretoggle.Toggle
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import java.net.URI
 
 @ConfigurationProperties("funksjonsbrytere")
-@ConstructorBinding
 class FeatureToggleConfig(
     private val enabled: Boolean,
     private val unleash: Unleash,
 ) {
 
-    @ConstructorBinding
     data class Unleash(
         val uri: URI,
         val environment: String,
