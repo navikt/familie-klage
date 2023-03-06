947a81ddd43a6697225e261f82f22e467eabc924
Johan Blomgren <blommish@users.noreply.github.com>
2023-03-06 10:39:08 +0100

    Spring boot 3 (#264)
    * Oppdaterer pom til spring boot 3
    
    * Erstatter javax med jakarta
    
    * Fjerner @ConstructorBinding som nå er unødvendig sammen med @ConfigurationProperties
    
    * Oppdaterer application.yml
    Fjerner auto-time som nå er default
    
    * Oppdaterer wiremock til standalone pga trøbbel med spring boot 3
    
    * Må ta med jetty-server pga trøbbel med jarkarta servlet v6

--- src/main/kotlin/no/nav/familie/klage/infrastruktur/config/RolleConfig.kt
+++ src/main/kotlin/no/nav/familie/klage/infrastruktur/config/RolleConfig.kt
@@ -1,10 +1,8 @@
 package no.nav.familie.klage.infrastruktur.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties("rolle")
-@ConstructorBinding
 class RolleConfig(
     val ba: FagsystemRolleConfig,
     val ef: FagsystemRolleConfig,
