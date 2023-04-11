201865ca9819fba8cedf8648f52ec6a65a3acd8b
sarahjelle <sara.hjelle@bekk.no>
2023-04-11 15:35:24 +0200

    * Oppdatere oppgavebeskrivelse skal være patch * Refaktor. Navnendring på funksjon for å være mer beskrivende.

--- src/main/kotlin/no/nav/familie/klage/ekstern/EksternBehandlingController.kt
+++ src/main/kotlin/no/nav/familie/klage/ekstern/EksternBehandlingController.kt
@@ -15,6 +15,7 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -67,8 +68,8 @@ class EksternBehandlingController(
         opprettBehandlingService.opprettBehandling(opprettKlageBehandlingDto)
     }
 
-    @PostMapping("/behandling/{behandlingId}/gjelder-tilbakekreving")
-    fun oppdaterOppgaveForBehandling(@PathVariable behandlingId: UUID) {
+    @PatchMapping("/behandling/{behandlingId}/gjelder-tilbakekreving")
+    fun oppdaterOppgaveTilÅGjeldeTilbakekreving(@PathVariable behandlingId: UUID) {
         oppgaveService.oppdaterOppgaveTilÅGjeldeTilbakekreving(behandlingId)
     }
 }
