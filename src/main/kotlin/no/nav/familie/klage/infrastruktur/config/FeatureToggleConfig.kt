b6a2045b9721f85aa9d916e0dfbe6a0bc992120f
throndi <56258085+throndi@users.noreply.github.com>
2023-09-13 22:23:15 +0200

    Fjerner Unleash og legger til Unleash Next fra felles

--- src/main/kotlin/no/nav/familie/klage/infrastruktur/config/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/klage/infrastruktur/config/FeatureToggleConfig.kt
@@ -1,90 +1,27 @@
 package no.nav.familie.klage.infrastruktur.config
 
-import io.getunleash.DefaultUnleash
-import io.getunleash.UnleashContext
-import io.getunleash.UnleashContextProvider
-import io.getunleash.util.UnleashConfig
+import io.getunleash.strategy.Strategy
 import no.nav.familie.klage.infrastruktur.featuretoggle.ByEnvironmentStrategy
 import no.nav.familie.klage.infrastruktur.featuretoggle.ByUserIdStrategy
-import no.nav.familie.klage.infrastruktur.featuretoggle.FeatureToggleService
-import no.nav.familie.klage.infrastruktur.featuretoggle.Toggle
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.boot.context.properties.ConfigurationProperties
+import no.nav.familie.unleash.DefaultUnleashService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
-import java.net.URI
+import org.springframework.context.annotation.Configuration
 
-@ConfigurationProperties("funksjonsbrytere")
+@Configuration
 class FeatureToggleConfig(
-    private val enabled: Boolean,
-    private val unleash: Unleash,
+    @Value("\${UNLEASH_SERVER_API_URL}") private val apiUrl: String,
+    @Value("\${UNLEASH_SERVER_API_TOKEN}") private val apiToken: String,
+    @Value("\${NAIS_APP_NAME}") private val appName: String,
 ) {
 
-    data class Unleash(
-        val uri: URI,
-        val environment: String,
-        val applicationName: String,
-    )
-
-    private val log: Logger = LoggerFactory.getLogger(this::class.java)
+    @Bean
+    fun strategies(): List<Strategy> {
+        return listOf(ByUserIdStrategy(), ByEnvironmentStrategy())
+    }
 
     @Bean
-    fun featureToggle(): FeatureToggleService =
-        if (enabled) {
-            lagUnleashFeatureToggleService()
-        } else {
-            log.warn(
-                "Funksjonsbryter-funksjonalitet er skrudd AV. " +
-                    "Gir standardoppførsel for alle funksjonsbrytere, dvs 'false'",
-            )
-            lagDummyFeatureToggleService()
-        }
-
-    private fun lagUnleashFeatureToggleService(): FeatureToggleService {
-        val unleash = DefaultUnleash(
-            UnleashConfig.builder()
-                .appName(unleash.applicationName)
-                .unleashAPI(unleash.uri)
-                .unleashContextProvider(lagUnleashContextProvider())
-                .build(),
-            ByEnvironmentStrategy(),
-            ByUserIdStrategy(),
-        )
-
-        return object : FeatureToggleService {
-            override fun isEnabled(toggle: Toggle, defaultValue: Boolean): Boolean {
-                return unleash.isEnabled(toggle.toggleId, defaultValue)
-            }
-
-            // Spring trigger denne ved shutdown. Gjøres for å unngå at unleash fortsetter å gjøre kall ut
-            override fun destroy() {
-                unleash.shutdown()
-            }
-        }
-    }
-
-    private fun lagUnleashContextProvider(): UnleashContextProvider {
-        return UnleashContextProvider {
-            UnleashContext.builder()
-                // .userId("a user") // Må legges til en gang i fremtiden
-                .environment(unleash.environment)
-                .appName(unleash.applicationName)
-                .build()
-        }
-    }
-
-    private fun lagDummyFeatureToggleService(): FeatureToggleService {
-        return object : FeatureToggleService {
-            override fun isEnabled(toggle: Toggle, defaultValue: Boolean): Boolean {
-                if (unleash.environment == "local") {
-                    return true
-                }
-                return defaultValue
-            }
-
-            override fun destroy() {
-                // Dummy featureToggleService trenger ikke destroy, då den ikke har en unleash å lukke
-            }
-        }
+    fun defaultUnleashService(strategies: List<Strategy>): DefaultUnleashService {
+        return DefaultUnleashService(apiUrl, apiToken, appName, strategies)
     }
 }
