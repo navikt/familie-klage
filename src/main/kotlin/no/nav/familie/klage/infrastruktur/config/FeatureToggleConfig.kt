2e55e593b76d5f0f394dc77a03f7bf4d21d86557
throndi <56258085+throndi@users.noreply.github.com>
2023-09-28 10:23:48 +0200

    Fjerner oppbønning av UnleashService i FeatureToggleConfig for å kunne bruke Unleash enabled switch til å disable

--- src/main/kotlin/no/nav/familie/klage/infrastruktur/config/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/klage/infrastruktur/config/FeatureToggleConfig.kt
@@ -3,25 +3,14 @@ package no.nav.familie.klage.infrastruktur.config
 import io.getunleash.strategy.Strategy
 import no.nav.familie.klage.infrastruktur.featuretoggle.ByEnvironmentStrategy
 import no.nav.familie.klage.infrastruktur.featuretoggle.ByUserIdStrategy
-import no.nav.familie.unleash.DefaultUnleashService
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class FeatureToggleConfig(
-    @Value("\${UNLEASH_SERVER_API_URL}") private val apiUrl: String,
-    @Value("\${UNLEASH_SERVER_API_TOKEN}") private val apiToken: String,
-    @Value("\${NAIS_APP_NAME}") private val appName: String,
-) {
+class FeatureToggleConfig {
 
     @Bean
     fun strategies(): List<Strategy> {
         return listOf(ByUserIdStrategy(), ByEnvironmentStrategy())
-    }
-
-    @Bean
-    fun defaultUnleashService(strategies: List<Strategy>): DefaultUnleashService {
-        return DefaultUnleashService(apiUrl, apiToken, appName, strategies)
     }
 }
