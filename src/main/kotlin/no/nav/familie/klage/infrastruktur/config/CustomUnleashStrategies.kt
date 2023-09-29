41bc43293b6af406ebce49a8f6d599e01def9a5a
throndi <56258085+throndi@users.noreply.github.com>
2023-09-29 12:18:20 +0200

    Endrer på navn fra FeatureToggleConfig til CustomUnleashStrategies

--- src/main/kotlin/no/nav/familie/klage/infrastruktur/config/CustomUnleashStrategies.kt
+++ src/main/kotlin/no/nav/familie/klage/infrastruktur/config/CustomUnleashStrategies.kt
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class FeatureToggleConfig {
+class CustomUnleashStrategies {
 
     @Bean
     fun strategies(): List<Strategy> {
