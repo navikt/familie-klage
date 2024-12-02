c9040a4b84efdbdbb6bd0e17fd805f9d6f376884
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2024-12-02 13:11:20 +0100

    Legg til NFP i brevsignatur hvis fagsystem er BA/KS

--- src/test/kotlin/no/nav/familie/klage/brev/BrevsignaturServiceTest.kt
+++ src/test/kotlin/no/nav/familie/klage/brev/BrevsignaturServiceTest.kt
@@ -5,11 +5,11 @@ import io.mockk.mockk
 import no.nav.familie.klage.personopplysninger.dto.Adressebeskyttelse
 import no.nav.familie.klage.personopplysninger.dto.PersonopplysningerDto
 import no.nav.familie.klage.testutil.BrukerContextUtil.testWithBrukerContext
+import no.nav.familie.kontrakter.felles.klage.Fagsystem
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class BrevsignaturServiceTest {
-
     val brevsignaturService = BrevsignaturService()
 
     @Test
@@ -17,7 +17,7 @@ internal class BrevsignaturServiceTest {
         val personopplysningerDto = mockk<PersonopplysningerDto>()
         every { personopplysningerDto.adressebeskyttelse } returns Adressebeskyttelse.STRENGT_FORTROLIG
 
-        val signaturMedEnhet = brevsignaturService.lagSignatur(personopplysningerDto)
+        val signaturMedEnhet = brevsignaturService.lagSignatur(personopplysningerDto, Fagsystem.EF)
 
         assertThat(signaturMedEnhet.enhet).isEqualTo(BrevsignaturService.ENHET_VIKAFOSSEN)
         assertThat(signaturMedEnhet.navn).isEqualTo(BrevsignaturService.NAV_ANONYM_NAVN)
@@ -29,10 +29,34 @@ internal class BrevsignaturServiceTest {
         every { personopplysningerDto.adressebeskyttelse } returns null
 
         val signaturMedEnhet = testWithBrukerContext(preferredUsername = "Julenissen") {
-            brevsignaturService.lagSignatur(personopplysningerDto)
+            brevsignaturService.lagSignatur(personopplysningerDto, Fagsystem.EF)
         }
 
         assertThat(signaturMedEnhet.enhet).isEqualTo(BrevsignaturService.ENHET_NAY)
         assertThat(signaturMedEnhet.navn).isEqualTo("Julenissen")
+    }
+
+    @Test
+    fun `skal sette riktig enhet i signatur basert på fagsystem`() {
+        val personopplysningerDto = mockk<PersonopplysningerDto>()
+        every { personopplysningerDto.adressebeskyttelse } returns null
+
+        val signaturForEnsligForsørger =
+            testWithBrukerContext {
+                brevsignaturService.lagSignatur(personopplysningerDto, Fagsystem.EF)
+            }
+        assertThat(signaturForEnsligForsørger.enhet).isEqualTo(BrevsignaturService.ENHET_NAY)
+
+        val signaturForBarnetrygd =
+            testWithBrukerContext {
+                brevsignaturService.lagSignatur(personopplysningerDto, Fagsystem.BA)
+            }
+        assertThat(signaturForBarnetrygd.enhet).isEqualTo(BrevsignaturService.ENHET_NFP)
+
+        val signaturForKontantstøtte =
+            testWithBrukerContext {
+                brevsignaturService.lagSignatur(personopplysningerDto, Fagsystem.KS)
+            }
+        assertThat(signaturForKontantstøtte.enhet).isEqualTo(BrevsignaturService.ENHET_NFP)
     }
 }
