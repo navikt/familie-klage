d008d00049186f876f14ee1f59c68f371bb28479
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-12-17 14:28:10 +0100

    Bytt validering av navn ved dødsbo

--- src/main/kotlin/no/nav/familie/klage/brev/mottaker/BrevmottakerValidator.kt
+++ src/main/kotlin/no/nav/familie/klage/brev/mottaker/BrevmottakerValidator.kt
@@ -18,8 +18,8 @@ object BrevmottakerValidator {
         if (brevmottaker.mottakertype == Mottakertype.BRUKER_MED_UTENLANDSK_ADRESSE && brevmottaker.navn != brukerensNavn) {
             throw Feil("Ved bruker med utenlandsk adresse skal brevmottakerens navn være brukerens navn.")
         }
-        if (brevmottaker.mottakertype == Mottakertype.DØDSBO && brevmottaker.navn != brukerensNavn) {
-            throw Feil("Ved dødsbo skal brevmottakerens navn være brukerens navn.")
+        if (brevmottaker.mottakertype == Mottakertype.DØDSBO && !brevmottaker.navn.contains(brukerensNavn)) {
+            throw Feil("Ved dødsbo skal brevmottakerens navn inneholde brukerens navn.")
         }
         if (brevmottaker.mottakertype == Mottakertype.DØDSBO && eksisterendeBrevmottakere.isNotEmpty()) {
             throw Feil("Ved dødsbo kan det ikke være flere brevmottakere.")
