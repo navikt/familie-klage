48ed9e9a6fd0d84ab99387455329e4ce3e04c3c0
Kristian Kofoed <kristian.kofoed@nav.no>
2025-01-06 09:20:15 +0100

    Feature - mulighet for å sette behandling på vent gjennom klage  (#549)
    * WIP: Start med sett på vent. Laget endepunkt for å hente behandling.
    
    * Manglet elvis-operator.
    
    * Returnere null som default på prioritet og mappeId
    
    * Introduserer først utekast av taAvVent for behandling. Denne imlementasjonen er relativ primitiv.
    
    * Liten oppdatering på OppgaveService.kt. Introduserer nye metoder, der feltene til oppgave blir utfyllt. SettPåVent metode burde oppdater prioritet, frist og saksbehandler.
    
    * Fjernet en ubrukt konstant i oppdaterVerdierPåOppgave().
    
    * Fjernet ubrukte typer i Oppgave objekt i oppdaterVerdierPåOppgave metode.
    
    * Legger til oppgaveId på OppgaveDto.
    
    * Introduserte behandlingStatus i settPåVent metoder. Denne blir videre formidlet til oppdaterHistorikk, der denne verdien er optional. Usikker på om dette har en kritisk påvirkning.
    
    * Bommet på et kall som trengte BehandlingStatus. Setter denne til OPPRETTET midlertidlig, med en TODO for å forsikre om at vi går over denne siden.
    
    * La til beskrivelse, committer over to feil commits.
    
    * Revert "La til beskrivelse, committer over to feil commits."
    
    This reverts commit b8550c3ff44352922ffc5307f2738ee1424e7fc4.
    
    * Revert "Bommet på et kall som trengte BehandlingStatus. Setter denne til OPPRETTET midlertidlig, med en TODO for å forsikre om at vi går over denne siden."
    
    This reverts commit a43a57075b1b3cd738aedea7ba4a15408b77879d.
    
    * Revert "Introduserte behandlingStatus i settPåVent metoder. Denne blir videre formidlet til oppdaterHistorikk, der denne verdien er optional. Usikker på om dette har en kritisk påvirkning."
    
    This reverts commit 9e35dbd271fd269653885b185b436ada3784c84f.
    
    * La til beskrivelse i OppgaveDto.kt- Er mappet om i hentBeskrivelse.
    
    * Fjernet hard-kodet string for oppdatering av beskrivelse.
    
    * La til en fraFristDato string til OppgaveDto for å hindre bruker å sette en sett på vent frist tilbake i tid. Usikker på om dette burde ligge slik, spesielt når objektet har en fristFerdigstillelse dato. Endringene kan bli omittet.
    
    * Fjernet fraFristDato da dette ga uforventet oppførsel.
    
    * Prøvd å låne utled logikk fra EF-SAK for formatering av beskrivelse historikk tekst. Utelukker mappe tekst, da dette kommer siden.
    
    * Forsøk på å refaktorere litt kode for utledBehandling metoder. Kan være ting brekker.
    
    * Forsøk på å fikse utledning for utledTilordnetSaksbehandlerBeskrivelse slik at man dokumenterer når saksbehandler settes fra ingen til noen, samt noen til ingen.
    
    * Syntax endringer, hovedsaklig endret til å bruke call names på alle nye metoder. Har også gått over filer jeg egentlig ikke har rørt, men dessverre lintet over. Fjernet ubrukt BehandlingPåVentStatus kode.
    
    * Fjernet uheldig linting over Behandling.kt.
    
    * KT-lint og fjernet en TODO. Lagt til noen call names for noen metoder.
    
    * Fjernet endringer gjort i filer jeg ikke har hørt. Fjernet en glemt TODO funnet i hentOppgave.
    
    * Vi bruker nå validerHarSaksbehandlerrolleTilStønadForBehandling() i stede for validerHarVeilederrolleTilStønadForBehandling() i settPåVent metoden.
    
    * Fjernet call name fra bruerkfeil kall.
    
    * Endret hentOppgavet til å bruke tilordnetRessurs (for testing). Endret kanTaAvVent metode til å ikke sjekke for ferdigstilt.
    
    * Forsøk på å enkelt legge til feature toggle for settpåvent funksjonalitet. Kan være denne commiten revertes.
    
    ---------
    
    Co-authored-by: gunnsteingarmo <gunnstein.hoj.garmo@nav.no>

--- src/main/kotlin/no/nav/familie/klage/oppgave/TilordnetRessursService.kt
+++ src/main/kotlin/no/nav/familie/klage/oppgave/TilordnetRessursService.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.klage.oppgave
 
+import no.nav.familie.klage.behandling.dto.OppgaveDto
 import no.nav.familie.klage.infrastruktur.featuretoggle.FeatureToggleService
 import no.nav.familie.klage.infrastruktur.featuretoggle.Toggle
 import no.nav.familie.klage.infrastruktur.sikkerhet.SikkerhetContext
@@ -9,7 +10,7 @@ import no.nav.familie.kontrakter.felles.Tema
 import no.nav.familie.kontrakter.felles.oppgave.Oppgave
 import no.nav.familie.kontrakter.felles.oppgave.StatusEnum
 import org.springframework.stereotype.Service
-import java.util.UUID
+import java.util.*
 
 @Service
 class TilordnetRessursService(
@@ -30,6 +31,24 @@ class TilordnetRessursService(
             fornavn = saksbehandler?.fornavn ?: "",
             rolle = rolle,
         )
+    }
+
+    fun hentOppgave(behandlingId: UUID): OppgaveDto? {
+        val behandleSakOppgave = behandleSakOppgaveRepository.findByBehandlingId(behandlingId)
+        val oppgave = behandleSakOppgave?.let { oppgaveClient.finnOppgaveMedId(it.oppgaveId) }
+
+        return if (oppgave != null) {
+            OppgaveDto(
+                oppgaveId = oppgave.id,
+                beskrivelse = oppgave.beskrivelse,
+                tilordnetRessurs = oppgave.tilordnetRessurs ?: "",
+                prioritet = oppgave.prioritet,
+                fristFerdigstillelse = oppgave.fristFerdigstillelse ?: "",
+                mappeId = oppgave.mappeId,
+            )
+        } else {
+            null
+        }
     }
 
     private fun utledSaksbehandlerRolle(oppgave: Oppgave?): SaksbehandlerRolle {
