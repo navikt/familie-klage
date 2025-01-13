316018a429e86d330717dcebfa239f94a544c5f6
krikof-nav <kristian.kofoed@nav.no>
2025-01-13 10:00:47 +0100

    Tar med en liten statisk liste av typen MappeDto for testing. La til en TODO på FeatureToggleTestUtil, siden vi kan deligere ansvaret til den nye FeatureToggleMocken.

--- src/test/kotlin/no/nav/familie/klage/testutil/FeatureToggleTestUtil.kt
+++ src/test/kotlin/no/nav/familie/klage/testutil/FeatureToggleTestUtil.kt
@@ -4,6 +4,7 @@ import io.mockk.every
 import io.mockk.mockk
 import no.nav.familie.klage.infrastruktur.featuretoggle.FeatureToggleService
 
+// TODO: Ansvaret er nå deligert til FeatureToggleMock. Endre denne.
 fun mockFeatureToggleService(enabled: Boolean = true): FeatureToggleService {
     val mockk = mockk<FeatureToggleService>()
     every { mockk.isEnabled(any()) } returns enabled
