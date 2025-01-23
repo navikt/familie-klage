2e86df532982c911baafb0d3a2b0a9de57144da3
Magnus Tønnessen <magnustonnessen@gmail.com>
2025-01-23 11:10:42 +0100

    Legg til OversendtKlageAnkeV4

---
+++ src/main/kotlin/no/nav/familie/klage/kabal/domain/OversendtKlageAnkeV4.kt
@@ -0,0 +1,48 @@
+package no.nav.familie.klage.kabal
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class OversendtKlageAnkeV4(
+    val type: OversendtType,
+    val sakenGjelder: OversendtPartV4,
+    val klager: OversendtPartV4?,
+    val prosessfullmektig: OversendtProsessfullmektigV4?,
+    val fagsak: OversendtSak,
+    val kildeReferanse: String,
+    val dvhReferanse: String? = null,
+    val hjemler: List<KabalHjemmel>,
+    val forrigeBehandlendeEnhet: String,
+    val tilknyttedeJournalposter: List<OversendtDokumentReferanse>,
+    val brukersKlageMottattVedtaksinstans: LocalDate?,
+    val frist: LocalDate?,
+    val sakMottattKaTidspunkt: LocalDateTime?,
+    val ytelse: Ytelse,
+    val kommentar: String? = null,
+    val hindreAutomatiskSvarbrev: Boolean?,
+    val saksbehandlerIdentForTildeling: String?,
+)
+
+enum class OversendtType {
+    KLAGE,
+    ANKE,
+}
+
+data class OversendtProsessfullmektigV4(
+    val id: OversendtPartId?,
+    val navn: String?,
+    val adresse: OversendtAdresseV4?,
+)
+
+data class OversendtAdresseV4(
+    val adresselinje1: String?,
+    val adresselinje2: String?,
+    val adresselinje3: String?,
+    val postnummer: String?,
+    val poststed: String?,
+    val land: String,
+)
+
+data class OversendtPartV4(
+    val id: OversendtPartId,
+)
