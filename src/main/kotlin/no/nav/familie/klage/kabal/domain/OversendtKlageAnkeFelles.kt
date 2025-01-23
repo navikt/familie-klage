748904a598ae2772a1440a33cfedef5aa6a73d80
Magnus Tønnessen <magnus.tonnessen@nav.no>
2025-01-23 12:46:35 +0100

    Legg til OversendtKlageAnkeV4 (#573)
    * Legg til OversendtKlageAnkeV4
    
    * Trekk felles klasser for V3 og V4 ut i egen fil

---
+++ src/main/kotlin/no/nav/familie/klage/kabal/domain/OversendtKlageAnkeFelles.kt
@@ -0,0 +1,39 @@
+package no.nav.familie.klage.kabal.domain
+
+import no.nav.familie.kontrakter.felles.Fagsystem
+
+enum class OversendtPartIdType {
+    PERSON,
+    VIRKSOMHET,
+}
+
+data class OversendtPartId(
+    val type: OversendtPartIdType,
+    val verdi: String,
+)
+
+data class OversendtSak(
+    val fagsakId: String? = null,
+    val fagsystem: Fagsystem,
+)
+
+data class OversendtDokumentReferanse(
+    val type: MottakDokumentType,
+    val journalpostId: String,
+)
+
+enum class MottakDokumentType {
+    BRUKERS_SOEKNAD,
+    OPPRINNELIG_VEDTAK,
+    BRUKERS_KLAGE,
+    BRUKERS_ANKE,
+    OVERSENDELSESBREV,
+    KLAGE_VEDTAK,
+    ANNET,
+}
+
+enum class Ytelse {
+    ENF_ENF,
+    BAR_BAR,
+    KON_KON,
+}
