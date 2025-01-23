3a620ebc69484d7dadf4707e6b1dba3a5b220592
thoalm <thoalm@outlook.com>
2025-01-23 18:32:56 +0100

    Forenklet deseraliseringen av BrevmottakerPerson

--- src/main/kotlin/no/nav/familie/klage/brev/domain/Brev.kt
+++ src/main/kotlin/no/nav/familie/klage/brev/domain/Brev.kt
@@ -1,13 +1,7 @@
 package no.nav.familie.klage.brev.domain
 
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonDeserializer
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import no.nav.familie.klage.felles.domain.Fil
 import no.nav.familie.klage.felles.domain.Sporbar
-import no.nav.familie.klage.infrastruktur.config.ObjectMapperProvider.objectMapper
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Embedded
 import java.util.UUID
@@ -45,61 +39,4 @@ data class BrevmottakereJournalpostUtenIdent(
 sealed interface BrevmottakereJournalpost {
     val journalpostId: String
     val distribusjonId: String?
-}
-
-data class Brevmottakere(
-    val personer: List<BrevmottakerPerson> = emptyList(),
-    val organisasjoner: List<BrevmottakerOrganisasjon> = emptyList(),
-)
-
-enum class MottakerRolle {
-    BRUKER,
-    VERGE,
-    FULLMAKT,
-    BRUKER_MED_UTENLANDSK_ADRESSE,
-    DØDSBO,
-}
-
-@JsonDeserialize(`as` = BrevmottakerPersonMedIdent::class)
-data class BrevmottakerPersonMedIdent(
-    val personIdent: String,
-    override val mottakerRolle: MottakerRolle,
-    override val navn: String,
-) : BrevmottakerPerson
-
-@JsonDeserialize(`as` = BrevmottakerPersonUtenIdent::class)
-data class BrevmottakerPersonUtenIdent(
-    val id: UUID = UUID.randomUUID(),
-    override val mottakerRolle: MottakerRolle,
-    override val navn: String,
-    val adresselinje1: String,
-    val adresselinje2: String?,
-    val postnummer: String?,
-    val poststed: String?,
-    val landkode: String,
-) : BrevmottakerPerson
-
-@JsonDeserialize(using = BrevmottakerPersonDeserializer::class)
-sealed interface BrevmottakerPerson : Brevmottaker {
-    val navn: String
-    val mottakerRolle: MottakerRolle
-}
-
-data class BrevmottakerOrganisasjon(
-    val organisasjonsnummer: String,
-    val organisasjonsnavn: String,
-    val navnHosOrganisasjon: String,
-) : Brevmottaker
-
-sealed interface Brevmottaker
-
-class BrevmottakerPersonDeserializer : JsonDeserializer<BrevmottakerPerson>() {
-    override fun deserialize(jsonParser: JsonParser, context: DeserializationContext): BrevmottakerPerson {
-        val node = jsonParser.codec.readTree<JsonNode>(jsonParser)
-        return if (node.has("personIdent")) {
-            objectMapper.treeToValue(node, BrevmottakerPersonMedIdent::class.java)
-        } else {
-            objectMapper.treeToValue(node, BrevmottakerPersonUtenIdent::class.java)
-        }
-    }
 }
---
+++ src/test/kotlin/no/nav/familie/klage/brev/domain/BrevmottakerPersonDeserializerTest.kt
@@ -0,0 +1,107 @@
+package no.nav.familie.klage.brev.domain
+
+import no.nav.familie.klage.infrastruktur.config.ObjectMapperProvider.objectMapper
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class BrevmottakerPersonDeserializerTest {
+    private val brevmottakerPersonDeserializer: BrevmottakerPersonDeserializer = BrevmottakerPersonDeserializer()
+
+    @Nested
+    inner class DeserializTest {
+        @Test
+        fun `skal deserialisere BrevmottakerPersonMedIdent`() {
+            // Arrange
+            val brevmottakerPersonMedIdent = BrevmottakerPersonMedIdent(
+                personIdent = "01492350318",
+                mottakerRolle = MottakerRolle.BRUKER,
+                navn = "Fornavn mellomnavn Etternavn",
+            )
+
+            val json = objectMapper.writeValueAsString(brevmottakerPersonMedIdent)
+            val parser = objectMapper.factory.createParser(json)
+
+            // Act
+            val deserialize = brevmottakerPersonDeserializer.deserialize(parser, objectMapper.deserializationContext)
+
+            // Assert
+            assertThat(deserialize).isInstanceOfSatisfying(BrevmottakerPersonMedIdent::class.java) {
+                assertThat(it.personIdent).isEqualTo(brevmottakerPersonMedIdent.personIdent)
+                assertThat(it.mottakerRolle).isEqualTo(brevmottakerPersonMedIdent.mottakerRolle)
+                assertThat(it.navn).isEqualTo(brevmottakerPersonMedIdent.navn)
+            }
+        }
+
+        @Test
+        fun `skal deserialisere en norsk BrevmottakerPersonUtenIdent`() {
+            // Arrange
+            val id = UUID.randomUUID()
+
+            val brevmottakerPersonMedIdent = BrevmottakerPersonUtenIdent(
+                id = id,
+                mottakerRolle = MottakerRolle.FULLMAKT,
+                navn = "navn",
+                adresselinje1 = "Adresselinje 1",
+                adresselinje2 = "Adresselinje 2",
+                postnummer = "0010",
+                poststed = "Oslo",
+                landkode = "NO",
+            )
+
+            val json = objectMapper.writeValueAsString(brevmottakerPersonMedIdent)
+            val parser = objectMapper.factory.createParser(json)
+
+            // Act
+            val deserialize = brevmottakerPersonDeserializer.deserialize(parser, objectMapper.deserializationContext)
+
+            // Assert
+            assertThat(deserialize).isInstanceOfSatisfying(BrevmottakerPersonUtenIdent::class.java) {
+                assertThat(it.id).isEqualTo(id)
+                assertThat(it.mottakerRolle).isEqualTo(brevmottakerPersonMedIdent.mottakerRolle)
+                assertThat(it.navn).isEqualTo(brevmottakerPersonMedIdent.navn)
+                assertThat(it.adresselinje1).isEqualTo(brevmottakerPersonMedIdent.adresselinje1)
+                assertThat(it.adresselinje2).isEqualTo(brevmottakerPersonMedIdent.adresselinje2)
+                assertThat(it.postnummer).isEqualTo(brevmottakerPersonMedIdent.postnummer)
+                assertThat(it.poststed).isEqualTo(brevmottakerPersonMedIdent.poststed)
+                assertThat(it.landkode).isEqualTo(brevmottakerPersonMedIdent.landkode)
+            }
+        }
+
+        @Test
+        fun `skal deserialisere en utenlandsk BrevmottakerPersonUtenIdent`() {
+            // Arrange
+            val id = UUID.randomUUID()
+
+            val brevmottakerPersonMedIdent = BrevmottakerPersonUtenIdent(
+                id = id,
+                mottakerRolle = MottakerRolle.BRUKER_MED_UTENLANDSK_ADRESSE,
+                navn = "navn",
+                adresselinje1 = "Adresselinje 1, København, 999",
+                adresselinje2 = "Adresselinje 1, København, 999",
+                postnummer = null,
+                poststed = null,
+                landkode = "DK",
+            )
+
+            val json = objectMapper.writeValueAsString(brevmottakerPersonMedIdent)
+            val parser = objectMapper.factory.createParser(json)
+
+            // Act
+            val deserialize = brevmottakerPersonDeserializer.deserialize(parser, objectMapper.deserializationContext)
+
+            // Assert
+            assertThat(deserialize).isInstanceOfSatisfying(BrevmottakerPersonUtenIdent::class.java) {
+                assertThat(it.id).isEqualTo(id)
+                assertThat(it.mottakerRolle).isEqualTo(brevmottakerPersonMedIdent.mottakerRolle)
+                assertThat(it.navn).isEqualTo(brevmottakerPersonMedIdent.navn)
+                assertThat(it.adresselinje1).isEqualTo(brevmottakerPersonMedIdent.adresselinje1)
+                assertThat(it.adresselinje2).isEqualTo(brevmottakerPersonMedIdent.adresselinje2)
+                assertThat(it.postnummer).isEqualTo(brevmottakerPersonMedIdent.postnummer)
+                assertThat(it.poststed).isEqualTo(brevmottakerPersonMedIdent.poststed)
+                assertThat(it.landkode).isEqualTo(brevmottakerPersonMedIdent.landkode)
+            }
+        }
+    }
+}
