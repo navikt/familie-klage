2f2987f39f912f9bc1d80f404d5d49c264703f62
thoalm <thoalm@outlook.com>
2025-01-25 14:13:45 +0100

    Opprettet testfil for BrevmottakerSletter

---
+++ src/test/kotlin/no/nav/familie/klage/brevmottaker/BrevmottakerSletterTest.kt
@@ -0,0 +1,44 @@
+package no.nav.familie.klage.brevmottaker
+
+import io.mockk.mockk
+import no.nav.familie.klage.behandling.BehandlingService
+import no.nav.familie.klage.brev.BrevRepository
+import no.nav.familie.klage.brev.BrevService
+import no.nav.familie.klage.fagsak.FagsakService
+import no.nav.familie.klage.personopplysninger.PersonopplysningerService
+import no.nav.familie.klage.testutil.DomainUtil
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+// TODO : Finish writing tests
+class BrevmottakerSletterTest {
+    private val behandlingService: BehandlingService = mockk()
+    private val fagsakService: FagsakService = mockk()
+    private val brevService: BrevService = mockk()
+    private val brevRepository: BrevRepository = mockk()
+    private val personopplysningerService: PersonopplysningerService = mockk()
+    private val brevmottakerSletter: BrevmottakerSletter = BrevmottakerSletter(
+        behandlingService = behandlingService,
+        brevService = brevService,
+        brevRepository = brevRepository,
+        fagsakService = fagsakService,
+        personopplysningerService = personopplysningerService,
+    )
+
+    @Nested
+    inner class SlettBrevmottakerTest {
+        @Test
+        fun `skal slette brevmottaker`() {
+            // Arrange
+            val behandling = DomainUtil.behandling()
+
+            // Act
+            brevmottakerSletter.slettBrevmottaker(behandling.id, UUID.randomUUID())
+
+            // Assert
+            assertThat(1).isEqualTo(1)
+        }
+    }
+}
