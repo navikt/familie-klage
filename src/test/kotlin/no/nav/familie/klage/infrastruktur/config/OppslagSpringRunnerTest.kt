0f57433e2205bd3c13aa89e08e000a561a902067
thoalm <thoalm@outlook.com>
2025-02-07 09:23:42 +0100

    NAV-23920: Sletter ubrukte tabeller

--- src/test/kotlin/no/nav/familie/klage/infrastruktur/config/OppslagSpringRunnerTest.kt
+++ src/test/kotlin/no/nav/familie/klage/infrastruktur/config/OppslagSpringRunnerTest.kt
@@ -107,7 +107,6 @@ abstract class OppslagSpringRunnerTest {
 
     private fun resetDatabase() {
         listOf(
-            Brevmottaker::class,
             BehandleSakOppgave::class,
             Behandlingshistorikk::class,
             Avsnitt::class,
