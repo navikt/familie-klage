573b250018c13f526994460aa0eb8c5bc384e3b4
endrehm <endre.heien.mo@nav.no>
2025-02-11 12:24:50 +0100

    Lagt til sjekk for verge og fullmakt i controller og tilhørende tester

--- src/main/kotlin/no/nav/familie/klage/henlegg/HenleggBehandlingController.kt
+++ src/main/kotlin/no/nav/familie/klage/henlegg/HenleggBehandlingController.kt
@@ -2,9 +2,11 @@ package no.nav.familie.klage.henlegg
 
 import no.nav.familie.klage.brev.BrevService
 import no.nav.familie.klage.felles.domain.AuditLoggerEvent
+import no.nav.familie.klage.felles.util.isEqualOrAfter
 import no.nav.familie.klage.infrastruktur.exception.feilHvis
 import no.nav.familie.klage.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.familie.klage.infrastruktur.sikkerhet.TilgangService
+import no.nav.familie.klage.personopplysninger.PersonopplysningerService
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.klage.HenlagtÅrsak
 import no.nav.security.token.support.core.api.ProtectedWithClaims
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 import java.util.UUID
 
 @RestController
@@ -25,6 +28,7 @@ class HenleggBehandlingController(
     private val tilgangService: TilgangService,
     private val henleggBehandlingService: HenleggBehandlingService,
     private val brevService: BrevService,
+    private val personopplysningerService: PersonopplysningerService,
 ) {
 
     @PostMapping("{behandlingId}/henlegg")
@@ -33,6 +37,7 @@ class HenleggBehandlingController(
         tilgangService.validerHarSaksbehandlerrolleTilStønadForBehandling(behandlingId)
         if (henlegg.skalSendeHenleggelsesbrev) {
             validerIkkeSendBrevPåFeilType(henlegg)
+            validerIkkeSendBrevHvisVergemålEllerFullmakt(behandlingId)
             brevService.opprettJournalførHenleggelsesbrevTask(behandlingId)
         }
         return Ressurs.success(henleggBehandlingService.henleggBehandling(behandlingId, henlegg))
@@ -55,5 +60,25 @@ class HenleggBehandlingController(
 
     private fun validerIkkeSendBrevPåFeilType(henlagt: HenlagtDto) {
         feilHvis(henlagt.skalSendeHenleggelsesbrev && henlagt.årsak == HenlagtÅrsak.FEILREGISTRERT) { "Skal ikke sende brev hvis type er ulik trukket tilbake" }
+    }
+
+    private fun validerIkkeSendBrevHvisVergemålEllerFullmakt(
+        ident: UUID,
+    ) {
+        val personopplysninger = personopplysningerService.hentPersonopplysninger(ident)
+        val harVerge = personopplysninger.vergemål.isNotEmpty()
+        val harFullmakt: Boolean =
+            personopplysninger.fullmakt
+                .filter {
+                    it.gyldigTilOgMed == null ||
+                        (
+                            it.gyldigTilOgMed.isEqualOrAfter(
+                                LocalDate.now(),
+                            )
+                            )
+                }.isNotEmpty()
+        feilHvis(harVerge || harFullmakt) {
+            "Skal ikke sende brev hvis person er tilknyttet vergemål eller fullmakt"
+        }
     }
 }
--- src/test/kotlin/no/nav/familie/klage/henlegg/HenleggBehandlingControllerTest.kt
+++ src/test/kotlin/no/nav/familie/klage/henlegg/HenleggBehandlingControllerTest.kt
@@ -1,14 +1,22 @@
 package no.nav.familie.klage.henlegg
 
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import no.nav.familie.klage.brev.BrevService
 import no.nav.familie.klage.infrastruktur.exception.Feil
 import no.nav.familie.klage.infrastruktur.sikkerhet.TilgangService
+import no.nav.familie.klage.personopplysninger.PersonopplysningerService
+import no.nav.familie.klage.personopplysninger.dto.FullmaktDto
+import no.nav.familie.klage.personopplysninger.dto.Kjønn
+import no.nav.familie.klage.personopplysninger.dto.PersonopplysningerDto
+import no.nav.familie.klage.personopplysninger.dto.VergemålDto
 import no.nav.familie.kontrakter.felles.klage.HenlagtÅrsak
 import no.nav.familie.prosessering.internal.TaskService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.time.LocalDate
 import java.util.UUID
 
 class HenleggBehandlingControllerTest {
@@ -16,11 +24,13 @@ class HenleggBehandlingControllerTest {
     val tilgangService = mockk<TilgangService>(relaxed = true)
     val henleggBehandlingService = mockk<HenleggBehandlingService>(relaxed = true)
     val brevService = mockk<BrevService>(relaxed = true)
+    val personopplysningerService = mockk<PersonopplysningerService>(relaxed = true)
 
     val henleggBehandlingController = HenleggBehandlingController(
         tilgangService = tilgangService,
         henleggBehandlingService = henleggBehandlingService,
         brevService = brevService,
+        personopplysningerService = personopplysningerService,
     )
 
     @Test
@@ -49,4 +59,91 @@ class HenleggBehandlingControllerTest {
         )
         verify(exactly = 0) { brevService.opprettJournalførHenleggelsesbrevTask(any()) }
     }
+
+    @Test internal fun `Skal kaste feil hvis bruker har fullmakt`() {
+        mocckHentPersonopplysningerMedFullmakt()
+        val exception =
+            assertThrows<Feil> {
+                henleggBehandlingController.henleggBehandling(UUID.randomUUID(), HenlagtDto(HenlagtÅrsak.TRUKKET_TILBAKE, true))
+            }
+        assertThat(exception.message).isEqualTo("Skal ikke sende brev hvis person er tilknyttet vergemål eller fullmakt")
+    }
+
+    @Test internal fun `Skal ikke kaste feil hvis bruker har fullmakt som har utgått`() {
+        mocckHentPersonopplysningerMedFullmaktEnDagSiden()
+        henleggBehandlingController.henleggBehandling(UUID.randomUUID(), HenlagtDto(årsak = HenlagtÅrsak.TRUKKET_TILBAKE, skalSendeHenleggelsesbrev = true))
+        verify(exactly = 1) { brevService.opprettJournalførHenleggelsesbrevTask(any()) }
+    }
+
+    @Test internal fun `Skal kaste feil hvis bruker har Verge`() {
+        mockkHentPersonopplysningerMedVergemål()
+        val exception =
+            assertThrows<Feil> {
+                henleggBehandlingController.henleggBehandling(UUID.randomUUID(), HenlagtDto(årsak = HenlagtÅrsak.TRUKKET_TILBAKE, skalSendeHenleggelsesbrev = true))
+            }
+        assertThat(exception.message).isEqualTo("Skal ikke sende brev hvis person er tilknyttet vergemål eller fullmakt")
+    }
+
+    private fun mocckHentPersonopplysningerMedFullmaktEnDagSiden() {
+        every { personopplysningerService.hentPersonopplysninger(any()) } returns
+            dto(
+                fullmakt =
+                listOf(
+                    FullmaktDto(
+                        gyldigFraOgMed = LocalDate.now().minusDays(2),
+                        gyldigTilOgMed = LocalDate.now().minusDays(1),
+                        navn = "123",
+                        motpartsPersonident = "123",
+                        områder = emptyList(),
+                    ),
+                ),
+            )
+    }
+
+    private fun mocckHentPersonopplysningerMedFullmakt() {
+        every { personopplysningerService.hentPersonopplysninger(any()) } returns
+            dto(
+                fullmakt =
+                listOf(
+                    FullmaktDto(
+                        gyldigFraOgMed = LocalDate.now(),
+                        gyldigTilOgMed = null,
+                        navn = "123",
+                        motpartsPersonident = "123",
+                        områder = emptyList(),
+                    ),
+                ),
+            )
+    }
+
+    private fun mockkHentPersonopplysningerMedVergemål() {
+        every { personopplysningerService.hentPersonopplysninger(any()) } returns
+            dto(
+                vergemål =
+                listOf(
+                    VergemålDto(
+                        embete = null,
+                        type = null,
+                        motpartsPersonident = null,
+                        navn = null,
+                        omfang = null,
+                    ),
+                ),
+            )
+    }
+
+    private fun dto(
+        fullmakt: List<FullmaktDto> = emptyList(),
+        vergemål: List<VergemålDto> = emptyList(),
+    ) = PersonopplysningerDto(
+        personIdent = "",
+        navn = "",
+        kjønn = Kjønn.MANN,
+        adressebeskyttelse = null,
+        folkeregisterpersonstatus = null,
+        dødsdato = null,
+        fullmakt = fullmakt,
+        egenAnsatt = false,
+        vergemål = vergemål,
+    )
 }
