c6da580fc9363f80ed4a7f20ad411b599856e547
Viktor Solberg <viktorgsolberg@gmail.com>
2025-02-27 11:19:16 +0100

    skal ikke sende brev eller brevmottakere med til kabal dersom klagebehandlingsårsaken er henvdelse fra kabal

--- src/main/kotlin/no/nav/familie/klage/distribusjon/SendTilKabalTask.kt
+++ src/main/kotlin/no/nav/familie/klage/distribusjon/SendTilKabalTask.kt
@@ -6,6 +6,7 @@ import no.nav.familie.klage.fagsak.FagsakService
 import no.nav.familie.klage.felles.util.TaskMetadata
 import no.nav.familie.klage.kabal.KabalService
 import no.nav.familie.klage.vurdering.VurderingService
+import no.nav.familie.kontrakter.felles.klage.Klagebehandlingsårsak
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
@@ -35,10 +36,18 @@ class SendTilKabalTask(
         val saksbehandlerIdent = task.metadata[TaskMetadata.saksbehandlerMetadataKey].toString()
         val behandling = behandlingService.hentBehandling(behandlingId)
         val fagsak = fagsakService.hentFagsakForBehandling(behandlingId)
+
         val vurdering =
             vurderingService.hentVurdering(behandlingId)
                 ?: error("Mangler vurdering på klagen - kan ikke oversendes til kabal")
-        val brevmottakere = brevService.hentBrevmottakere(behandlingId)
+
+        val brevmottakere =
+            if (behandling.årsak != Klagebehandlingsårsak.HENVENDELSE_FRA_KABAL) {
+                brevService.hentBrevmottakere(behandlingId)
+            } else {
+                null
+            }
+
         try {
             kabalService.sendTilKabal(fagsak, behandling, vurdering, saksbehandlerIdent, brevmottakere)
         } catch (e: HttpClientErrorException) {
