2f10e4487c9f48834652e0843ec503e5c9223d62
krikof-nav <kristian.kofoed@nav.no>
2025-03-13 13:00:48 +0100

    Slenger med logging bare for å sjekke verdier unleashContext gir.

--- src/main/kotlin/no/nav/familie/klage/infrastruktur/featuretoggle/ByTargetingStrategy.kt
+++ src/main/kotlin/no/nav/familie/klage/infrastruktur/featuretoggle/ByTargetingStrategy.kt
@@ -2,8 +2,12 @@ package no.nav.familie.klage.infrastruktur.featuretoggle
 
 import io.getunleash.UnleashContext
 import io.getunleash.strategy.Strategy
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class ByTargetingStrategy : Strategy {
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
     override fun getName() = "flexibleRollout"
 
     override fun isEnabled(
@@ -12,11 +16,17 @@ class ByTargetingStrategy : Strategy {
     ): Boolean {
         val currentUserId = unleashContext.userId.orElse(null)
 
+        logger.info("UNLEASH_CONTEXT_DEBUG --- unleashContext sier at unleashContext.userId er: $currentUserId.")
+        logger.info("UNLEASH_CONTEXT_DEBUG --- map som sendes med har en størrelse på: ${map.size}.s")
+        logger.info("UNLEASH_CONTEXT_DEBUG --- map som sendes med sine keys: ${map.keys} og verdier: ${map.values}.")
+
         return if (currentUserId != null) {
+            logger.info("UNLEASH_CONTEXT_DEBUG --- currentUserId er ikke null, men: $currentUserId.")
+            val mapVerdi = map[currentUserId]
+            logger.info("UNLEASH_CONTEXT_DEBUG --- verdi for currentUserId i map er: $mapVerdi.")
             map[currentUserId]?.toBoolean() ?: false
         } else {
             false
         }
     }
-
 }
