f205f0c214a286d680172ee42b0cb0470947a429
thoalm <thoalm@outlook.com>
2025-04-04 13:40:32 +0200

    NAV-24658: Ta i bruk nytt endepunkt for oppretting av revurdering klage for KS og BA

--- src/main/kotlin/no/nav/familie/klage/integrasjoner/FamilieBASakClient.kt
+++ src/main/kotlin/no/nav/familie/klage/integrasjoner/FamilieBASakClient.kt
@@ -12,31 +12,43 @@ import org.springframework.stereotype.Component
 import org.springframework.web.client.RestOperations
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
+import java.util.UUID
+import no.nav.familie.klage.infrastruktur.featuretoggle.FeatureToggleService
+import no.nav.familie.klage.infrastruktur.featuretoggle.Toggle
 
 @Component
 class FamilieBASakClient(
     @Qualifier("azure") restOperations: RestOperations,
     @Value("\${FAMILIE_BA_SAK_URL}") private val familieBaSakUri: URI,
+    private val featureToggleService: FeatureToggleService
 ) : AbstractRestClient(restOperations, "familie.ba.sak") {
 
-    fun hentVedtak(fagsystemEksternFagsakId: String): List<FagsystemVedtak> {
+    fun hentVedtak(eksternFagsakId: String): List<FagsystemVedtak> {
         val hentVedtakUri = UriComponentsBuilder.fromUri(familieBaSakUri)
-            .pathSegment("api/klage/fagsaker/$fagsystemEksternFagsakId/vedtak")
-            .build().toUri()
+            .pathSegment("api/klage/fagsaker/$eksternFagsakId/vedtak")
+            .build()
+            .toUri()
         return getForEntity<Ressurs<List<FagsystemVedtak>>>(hentVedtakUri).getDataOrThrow()
     }
 
-    fun kanOppretteRevurdering(fagsystemEksternFagsakId: String): KanOppretteRevurderingResponse {
+    fun kanOppretteRevurdering(eksternFagsakId: String): KanOppretteRevurderingResponse {
         val hentVedtakUri = UriComponentsBuilder.fromUri(familieBaSakUri)
-            .pathSegment("api/klage/fagsaker/$fagsystemEksternFagsakId/kan-opprette-revurdering-klage")
-            .build().toUri()
+            .pathSegment("api/klage/fagsaker/$eksternFagsakId/kan-opprette-revurdering-klage")
+            .build()
+            .toUri()
         return getForEntity<Ressurs<KanOppretteRevurderingResponse>>(hentVedtakUri).getDataOrThrow()
     }
 
-    fun opprettRevurdering(fagsystemEksternFagsakId: String): OpprettRevurderingResponse {
+    fun opprettRevurdering(eksternFagsakId: String, klagebehandlingId: UUID): OpprettRevurderingResponse {
+        val url = if (featureToggleService.isEnabled(Toggle.SEND_BEHANDLING_ID_VED_OPPRETTING_AV_REVURDERING_KLAGE)) {
+            "api/ekstern/fagsak/$eksternFagsakId/klagebehandling/$klagebehandlingId/opprett-revurdering-klage"
+        } else {
+            "api/ekstern/fagsaker/$eksternFagsakId/opprett-revurdering-klage"
+        }
         val hentVedtakUri = UriComponentsBuilder.fromUri(familieBaSakUri)
-            .pathSegment("api/klage/fagsaker/$fagsystemEksternFagsakId/opprett-revurdering-klage")
-            .build().toUri()
+            .pathSegment(url)
+            .build()
+            .toUri()
         return postForEntity<Ressurs<OpprettRevurderingResponse>>(hentVedtakUri, emptyMap<String, String>()).getDataOrThrow()
     }
 }
