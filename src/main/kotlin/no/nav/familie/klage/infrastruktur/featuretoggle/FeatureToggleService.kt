6a3926340a218a7fb8a39f599dfb073ff3b21ac1
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2025-04-23 11:46:28 +0200

    NAV-24658: Ta i bruk nytt endepunkt for oppretting av revurdering klage for KS og BA (#632)

--- src/main/kotlin/no/nav/familie/klage/infrastruktur/featuretoggle/FeatureToggleService.kt
+++ src/main/kotlin/no/nav/familie/klage/infrastruktur/featuretoggle/FeatureToggleService.kt
@@ -30,6 +30,10 @@ enum class Toggle(val toggleId: String, val beskrivelse: String? = null) {
     KAN_MELLOMLAGRE_VURDERING("familie-klage.kan-mellomlagre-vurdering", "Release"),
     SKAL_BRUKE_KABAL_API_V4("familie-klage.skal-bruke-kabal-api-v4", "Release"),
     SKAL_BRUKE_NY_LØYPE_FOR_JOURNALFØRING("familie-klage.skal-bruke-ny-loype-for-journalforing", "Release"),
+    SEND_BEHANDLING_ID_VED_OPPRETTING_AV_REVURDERING_KLAGE(
+        "familie-klage.send-behandlingid-ved-oppretting-av-revurdering-klage",
+        "Release"
+    )
     ;
 
     companion object {
