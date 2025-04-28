91e631981ae0ff8b9f8320b1d59c025490696468
endrehm <endre.heien.mo@nav.no>
2025-04-28 13:58:31 +0200

    Lagt til en test for sjekk at uventet enhetsnavn blir satt til standard NAY

--- src/test/kotlin/no/nav/familie/klage/brev/BrevsignaturServiceTest.kt
+++ src/test/kotlin/no/nav/familie/klage/brev/BrevsignaturServiceTest.kt
@@ -4,17 +4,25 @@ import io.mockk.every
 import io.mockk.mockk
 import no.nav.familie.klage.infrastruktur.featuretoggle.FeatureToggleService
 import no.nav.familie.klage.oppgave.OppgaveClient
+import no.nav.familie.klage.personopplysninger.PersonopplysningerService
 import no.nav.familie.klage.personopplysninger.dto.Adressebeskyttelse
 import no.nav.familie.klage.personopplysninger.dto.PersonopplysningerDto
+import no.nav.familie.klage.testutil.BrukerContextUtil
 import no.nav.familie.klage.testutil.BrukerContextUtil.testWithBrukerContext
 import no.nav.familie.kontrakter.felles.klage.Fagsystem
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
+import org.junit.jupiter.params.provider.ValueSource
+import no.nav.familie.kontrakter.felles.saksbehandler.Saksbehandler
+import java.util.UUID
 
 internal class BrevsignaturServiceTest {
-    val brevsignaturService = BrevsignaturService(mockk<FeatureToggleService>(relaxed = true), mockk<OppgaveClient>(relaxed = true))
+    private val personopplysningerService = mockk<PersonopplysningerService>()
+    private val oppgaveClient = mockk<OppgaveClient>()
+    val brevsignaturService = BrevsignaturService(mockk<FeatureToggleService>(relaxed = true), oppgaveClient)
 
     @Test
     fun `skal anonymisere signatur hvis strengt fortrolig`() {
@@ -46,6 +54,7 @@ internal class BrevsignaturServiceTest {
     fun `skal sette riktig enhet i brevsignatur basert på fagsystem`(fagsystem: Fagsystem) {
         val personopplysningerDto = mockk<PersonopplysningerDto>()
         every { personopplysningerDto.adressebeskyttelse } returns null
+        every { oppgaveClient.hentSaksbehandlerInfo(any()) } returns saksbehandler(enhetsnavn = "NAV ARBEID OG YTELSER UVENTET ENHET")
 
         val signaturForEnsligForsørger =
             testWithBrukerContext {
@@ -58,4 +67,44 @@ internal class BrevsignaturServiceTest {
             assertThat(signaturForEnsligForsørger.enhet).isEqualTo(BrevsignaturService.ENHET_NFP)
         }
     }
+
+    @Test
+    fun `skal sende frittstående brev med vanlig nay signatur uten geografisk lokasjon når enhetsnavn er uvanlig`() {
+        val saksbehandlerNavn = "Ole Olsen"
+        val personopplysningerDto = mockk<PersonopplysningerDto>()
+        BrukerContextUtil.mockBrukerContext(saksbehandlerNavn)
+
+        every { oppgaveClient.hentSaksbehandlerInfo(saksbehandlerNavn) } returns saksbehandler(enhetsnavn = "NAV ARBEID OG YTELSER UVENTET ENHET")
+        every { personopplysningerDto.adressebeskyttelse?.erStrengtFortrolig()} returns false
+        val signaturMedEnhet = brevsignaturService.lagSignatur(personopplysningerDto, Fagsystem.EF)
+
+        println(signaturMedEnhet.enhet)
+        Assertions.assertThat(signaturMedEnhet.enhet).isEqualTo(ENHET_NAY)
+        Assertions.assertThat(signaturMedEnhet.navn).isEqualTo(saksbehandlerNavn)
+
+        BrukerContextUtil.clearBrukerContext()
+    }
+
+    companion object {
+        val enhetsnavnTilVisningstekst =
+            mapOf(
+                "NAV ARBEID OG YTELSER SKIEN" to "Nav arbeid og ytelser Skien",
+                "NAV ARBEID OG YTELSER MØRE OG ROMSDAL" to "Nav arbeid og ytelser Møre og Romsdal",
+                "NAV ARBEID OG YTELSER SØRLANDET" to "Nav arbeid og ytelser Sørlandet",
+            )
+        val NAV_ANONYM_NAVN = "Nav anonym"
+        val ENHET_VIKAFOSSEN = "Nav Vikafossen"
+        val ENHET_NAY = "Nav arbeid og ytelser"
+        val ENHET_NFP = "Nav familie- og pensjonsytelser"
+    }
+
+    fun saksbehandler(enhetsnavn: String = "NAV ARBEID OG YTELSER SKIEN") =
+        Saksbehandler(
+            azureId = UUID.randomUUID(),
+            navIdent = "NAV123",
+            fornavn = "Darth",
+            etternavn = "Vader",
+            enhet = "4489",
+            enhetsnavn = enhetsnavn,
+        )
 }
