e5fff26ea0595d20df682f64332d78cafe22221a
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-11-24 11:03:34 +0100

    Send melding til statistikk når enhet endres

--- src/main/kotlin/no/nav/familie/klage/porteføljejustering/PorteføljejusteringController.kt
+++ src/main/kotlin/no/nav/familie/klage/porteføljejustering/PorteføljejusteringController.kt
@@ -4,9 +4,13 @@ import no.nav.familie.klage.behandling.BehandlingService
 import no.nav.familie.klage.behandling.enhet.Enhet.Companion.finnEnhet
 import no.nav.familie.klage.behandlingshistorikk.BehandlingshistorikkService
 import no.nav.familie.klage.behandlingshistorikk.domain.HistorikkHendelse
+import no.nav.familie.klage.behandlingsstatistikk.BehandlingsstatistikkTask.Companion.opprettPåbegyntTask
+import no.nav.familie.klage.fagsak.FagsakService
+import no.nav.familie.klage.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.familie.klage.oppgave.BehandleSakOppgaveRepository
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.klage.Fagsystem
+import no.nav.familie.prosessering.internal.TaskService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PutMapping
@@ -21,7 +25,9 @@ import org.springframework.web.bind.annotation.RestController
 class PorteføljejusteringController(
     private val behandleSakOppgaveRepository: BehandleSakOppgaveRepository,
     private val behandlingService: BehandlingService,
+    private val fagsakService: FagsakService,
     private val behandlingshistorikkService: BehandlingshistorikkService,
+    private val taskService: TaskService,
 ) {
     @PutMapping("/oppdater-behandlende-enhet")
     fun oppdaterBehandlendeEnhetPåBehandling(
@@ -36,6 +42,7 @@ class PorteføljejusteringController(
                 ?: throw IllegalStateException("Fant ikke BehandleSakOppgave for oppgaveId=$oppgaveId")
 
         val behandling = behandlingService.hentBehandling(behandleSakOppgave.behandlingId)
+        val fagsak = fagsakService.hentFagsak(behandling.fagsakId)
         val enhet = finnEnhet(fagsystem, nyEnhet)
 
         val enhetErOppdatert = enhet.enhetsnummer == behandling.behandlendeEnhet
@@ -54,6 +61,15 @@ class PorteføljejusteringController(
             steg = behandling.steg,
             historikkHendelse = HistorikkHendelse.BEHANDLENDE_ENHET_ENDRET,
             beskrivelse = "Behandlende enhet endret i forbindelse med porteføljejustering.",
+        )
+
+        taskService.save(
+            opprettPåbegyntTask(
+                behandlingId = behandling.id,
+                eksternFagsakId = fagsak.eksternId,
+                fagsystem = fagsystem,
+                gjeldendeSaksbehandler = SikkerhetContext.SYSTEM_FORKORTELSE,
+            ),
         )
 
         return Ressurs.success("Behandlende enhet oppdatert til ${enhet.enhetsnummer}.")
--- src/test/kotlin/no/nav/familie/klage/porteføljejustering/PorteføljejusteringControllerTest.kt
+++ src/test/kotlin/no/nav/familie/klage/porteføljejustering/PorteføljejusteringControllerTest.kt
@@ -1,21 +1,31 @@
 package no.nav.familie.klage.porteføljejustering
 
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.runs
+import io.mockk.slot
 import io.mockk.verify
 import no.nav.familie.klage.behandling.BehandlingService
 import no.nav.familie.klage.behandling.enhet.BarnetrygdEnhet
 import no.nav.familie.klage.behandling.enhet.KontantstøtteEnhet
 import no.nav.familie.klage.behandlingshistorikk.BehandlingshistorikkService
 import no.nav.familie.klage.behandlingshistorikk.domain.HistorikkHendelse
+import no.nav.familie.klage.behandlingsstatistikk.BehandlingsstatistikkHendelse
+import no.nav.familie.klage.behandlingsstatistikk.BehandlingsstatistikkTaskPayload
+import no.nav.familie.klage.fagsak.FagsakService
+import no.nav.familie.klage.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.familie.klage.oppgave.BehandleSakOppgave
 import no.nav.familie.klage.oppgave.BehandleSakOppgaveRepository
 import no.nav.familie.klage.porteføljejustering.PorteføljejusteringController.OppdaterBehandlendeEnhetRequest
 import no.nav.familie.klage.testutil.DomainUtil
+import no.nav.familie.klage.testutil.DomainUtil.fagsak
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.klage.Fagsystem
+import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.internal.TaskService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -25,21 +35,28 @@ import java.util.UUID
 class PorteføljejusteringControllerTest {
     private val behandleSakOppgaveRepository = mockk<BehandleSakOppgaveRepository>()
     private val behandlingService = mockk<BehandlingService>()
+    private val fagsakService = mockk<FagsakService>()
     private val behandlingshistorikkService = mockk<BehandlingshistorikkService>()
+    private val taskService = mockk<TaskService>()
 
     private val porteføljejusteringController =
         PorteføljejusteringController(
             behandleSakOppgaveRepository = behandleSakOppgaveRepository,
             behandlingService = behandlingService,
+            fagsakService = fagsakService,
             behandlingshistorikkService = behandlingshistorikkService,
+            taskService = taskService,
         )
 
+    private val fagsak = fagsak()
     private val behandlingId = UUID.randomUUID()
     private val oppgaveId = 123L
 
     @BeforeEach
     fun setup() {
+        every { fagsakService.hentFagsak(any()) } returns fagsak
         every { behandlingshistorikkService.opprettBehandlingshistorikk(any(), any(), any(), any()) } returns mockk()
+        every { taskService.save(any()) } returns mockk()
     }
 
     @Test
@@ -93,6 +110,7 @@ class PorteføljejusteringControllerTest {
 
         verify(exactly = 0) { behandlingService.oppdaterBehandlendeEnhet(any(), any(), any()) }
         verify(exactly = 0) { behandlingshistorikkService.opprettBehandlingshistorikk(any(), any(), any(), any()) }
+        verify(exactly = 0) { taskService.save(any()) }
     }
 
     @Test
@@ -124,6 +142,7 @@ class PorteføljejusteringControllerTest {
 
         verify(exactly = 0) { behandlingService.oppdaterBehandlendeEnhet(any(), any(), any()) }
         verify(exactly = 0) { behandlingshistorikkService.opprettBehandlingshistorikk(any(), any(), any(), any()) }
+        verify(exactly = 0) { taskService.save(any()) }
     }
 
     @Test
@@ -145,16 +164,24 @@ class PorteføljejusteringControllerTest {
                 fagsystem = Fagsystem.BA,
             )
 
+        val taskSlot = slot<Task>()
+
         every { behandleSakOppgaveRepository.findByOppgaveId(oppgaveId) } returns behandleSakOppgave
         every { behandlingService.hentBehandling(behandlingId) } returns behandling
         every { behandlingService.oppdaterBehandlendeEnhet(behandlingId, nyEnhet, Fagsystem.BA) } just runs
+        every { taskService.save(capture(taskSlot)) } returns mockk()
 
         // Act
         val result = porteføljejusteringController.oppdaterBehandlendeEnhetPåBehandling(request)
 
         // Assert
+        val lagretTaskPayload = objectMapper.readValue<BehandlingsstatistikkTaskPayload>(taskSlot.captured.payload)
+
         assertThat(result.status).isEqualTo(Ressurs.Status.SUKSESS)
         assertThat(result.data).isEqualTo("Behandlende enhet oppdatert til ${nyEnhet.enhetsnummer}.")
+        assertThat(lagretTaskPayload.behandlingId).isEqualTo(behandlingId)
+        assertThat(lagretTaskPayload.gjeldendeSaksbehandler).isEqualTo(SikkerhetContext.SYSTEM_FORKORTELSE)
+        assertThat(lagretTaskPayload.hendelse).isEqualTo(BehandlingsstatistikkHendelse.PÅBEGYNT)
 
         verify(exactly = 1) {
             behandlingService.oppdaterBehandlendeEnhet(
@@ -193,16 +220,24 @@ class PorteføljejusteringControllerTest {
                 fagsystem = Fagsystem.KS,
             )
 
+        val taskSlot = slot<Task>()
+
         every { behandleSakOppgaveRepository.findByOppgaveId(oppgaveId) } returns behandleSakOppgave
         every { behandlingService.hentBehandling(behandlingId) } returns behandling
         every { behandlingService.oppdaterBehandlendeEnhet(behandlingId, nyEnhet, Fagsystem.KS) } just runs
+        every { taskService.save(capture(taskSlot)) } returns mockk()
 
         // Act
         val result = porteføljejusteringController.oppdaterBehandlendeEnhetPåBehandling(request)
 
         // Assert
+        val lagretTaskPayload = objectMapper.readValue<BehandlingsstatistikkTaskPayload>(taskSlot.captured.payload)
+
         assertThat(result.status).isEqualTo(Ressurs.Status.SUKSESS)
         assertThat(result.data).isEqualTo("Behandlende enhet oppdatert til ${nyEnhet.enhetsnummer}.")
+        assertThat(lagretTaskPayload.behandlingId).isEqualTo(behandlingId)
+        assertThat(lagretTaskPayload.gjeldendeSaksbehandler).isEqualTo(SikkerhetContext.SYSTEM_FORKORTELSE)
+        assertThat(lagretTaskPayload.hendelse).isEqualTo(BehandlingsstatistikkHendelse.PÅBEGYNT)
 
         verify(exactly = 1) {
             behandlingService.oppdaterBehandlendeEnhet(
