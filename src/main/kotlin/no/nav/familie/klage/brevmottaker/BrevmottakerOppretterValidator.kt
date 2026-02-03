29090bfd5c19861b547fb0262b1b734bc11088e0
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2026-02-03 17:01:37 +0100

    Rydde litt

--- src/main/kotlin/no/nav/familie/klage/brevmottaker/BrevmottakerOppretterValidator.kt
+++ src/main/kotlin/no/nav/familie/klage/brevmottaker/BrevmottakerOppretterValidator.kt
@@ -1,7 +1,12 @@
 package no.nav.familie.klage.brevmottaker
 
 import no.nav.familie.klage.brevmottaker.domain.Brevmottaker
-import no.nav.familie.klage.brevmottaker.domain.MottakerRolle
+import no.nav.familie.klage.brevmottaker.domain.MottakerRolle.BRUKER
+import no.nav.familie.klage.brevmottaker.domain.MottakerRolle.BRUKER_MED_UTENLANDSK_ADRESSE
+import no.nav.familie.klage.brevmottaker.domain.MottakerRolle.DØDSBO
+import no.nav.familie.klage.brevmottaker.domain.MottakerRolle.FULLMAKT
+import no.nav.familie.klage.brevmottaker.domain.MottakerRolle.INSTITUSJON
+import no.nav.familie.klage.brevmottaker.domain.MottakerRolle.VERGE
 import no.nav.familie.klage.brevmottaker.domain.NyBrevmottakerOrganisasjon
 import no.nav.familie.klage.brevmottaker.domain.NyBrevmottakerPersonUtenIdent
 import no.nav.familie.klage.infrastruktur.exception.Feil
@@ -14,7 +19,7 @@ object BrevmottakerOppretterValidator {
         nyBrevmottakerPersonUtenIdent: NyBrevmottakerPersonUtenIdent,
         eksisterendeBrevmottakere: List<Brevmottaker>,
     ) {
-        val manueltOpprettedeBrevmottakere = eksisterendeBrevmottakere.filter { it.mottakerRolle !in setOf(MottakerRolle.BRUKER, MottakerRolle.INSTITUSJON) }
+        val manueltOpprettedeBrevmottakere = eksisterendeBrevmottakere.filter { it.mottakerRolle !in setOf(BRUKER, INSTITUSJON) }
         val eksisterendeMottakerRoller = manueltOpprettedeBrevmottakere.mapNotNull { it.mottakerRolle }
         when {
             eksisterendeMottakerRoller.any { it == nyBrevmottakerPersonUtenIdent.mottakerRolle } -> {
@@ -23,34 +28,34 @@ object BrevmottakerOppretterValidator {
                 )
             }
 
-            nyBrevmottakerPersonUtenIdent.mottakerRolle == MottakerRolle.BRUKER_MED_UTENLANDSK_ADRESSE &&
+            nyBrevmottakerPersonUtenIdent.mottakerRolle == BRUKER_MED_UTENLANDSK_ADRESSE &&
                 nyBrevmottakerPersonUtenIdent.navn != brukerensNavn -> {
                 throw Feil("Ved bruker med utenlandsk adresse skal brevmottakerens navn være brukerens navn for $behandlingId.")
             }
 
-            nyBrevmottakerPersonUtenIdent.mottakerRolle == MottakerRolle.DØDSBO &&
+            nyBrevmottakerPersonUtenIdent.mottakerRolle == DØDSBO &&
                 !nyBrevmottakerPersonUtenIdent.navn.contains(brukerensNavn) -> {
                 throw Feil("Ved dødsbo skal brevmottakerens navn inneholde brukerens navn for $behandlingId.")
             }
 
-            nyBrevmottakerPersonUtenIdent.mottakerRolle == MottakerRolle.DØDSBO &&
+            nyBrevmottakerPersonUtenIdent.mottakerRolle == DØDSBO &&
                 manueltOpprettedeBrevmottakere.isNotEmpty() -> {
                 throw Feil("Kan ikke legge til dødsbo når det allerede finnes brevmottakere for $behandlingId.")
             }
 
-            eksisterendeMottakerRoller.any { it == MottakerRolle.DØDSBO } -> {
+            eksisterendeMottakerRoller.any { it == DØDSBO } -> {
                 throw Feil("Kan ikke legge til flere brevmottakere når det allerede finnes et dødsbo for $behandlingId.")
             }
 
-            MottakerRolle.BRUKER_MED_UTENLANDSK_ADRESSE in eksisterendeMottakerRoller &&
-                nyBrevmottakerPersonUtenIdent.mottakerRolle !== MottakerRolle.VERGE &&
-                nyBrevmottakerPersonUtenIdent.mottakerRolle !== MottakerRolle.FULLMAKT -> {
+            BRUKER_MED_UTENLANDSK_ADRESSE in eksisterendeMottakerRoller &&
+                nyBrevmottakerPersonUtenIdent.mottakerRolle !== VERGE &&
+                nyBrevmottakerPersonUtenIdent.mottakerRolle !== FULLMAKT -> {
                 throw Feil("Bruker med utenlandsk adresse kan kun kombineres med verge eller fullmektig for $behandlingId.")
             }
 
             eksisterendeMottakerRoller.isNotEmpty() &&
-                MottakerRolle.BRUKER_MED_UTENLANDSK_ADRESSE !in eksisterendeMottakerRoller &&
-                nyBrevmottakerPersonUtenIdent.mottakerRolle !== MottakerRolle.BRUKER_MED_UTENLANDSK_ADRESSE -> {
+                    BRUKER_MED_UTENLANDSK_ADRESSE !in eksisterendeMottakerRoller &&
+                nyBrevmottakerPersonUtenIdent.mottakerRolle !== BRUKER_MED_UTENLANDSK_ADRESSE -> {
                 throw Feil("Kan kun legge til bruker med utenlandsk adresse om det finnes en brevmottaker allerede for $behandlingId.")
             }
         }
@@ -69,11 +74,11 @@ object BrevmottakerOppretterValidator {
                 )
             }
 
-            nyBrevmottakerOrganisasjon.mottakerRolle != MottakerRolle.FULLMAKT -> {
+            nyBrevmottakerOrganisasjon.mottakerRolle != FULLMAKT -> {
                 throw Feil("Organisasjon kan kun ha mottakerrolle fullmakt for $behandlingId.")
             }
 
-            eksisterendeMottakerRoller.any { it == MottakerRolle.INSTITUSJON } && eksisterendeBrevmottakere.size > 1 -> {
+            eksisterendeMottakerRoller.any { it == INSTITUSJON } && eksisterendeBrevmottakere.size > 1 -> {
                 throw Feil("Kan kun ha én ekstra brevmottaker når institusjon er brevmottaker for $behandlingId.")
             }
         }
