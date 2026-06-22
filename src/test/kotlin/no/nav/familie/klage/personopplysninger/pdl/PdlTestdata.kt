c38b975e71dc45aa1dfd3cb2ebb48b542961b089
thoalm <thoalm@outlook.com>
2026-06-22 10:31:13 +0200

    NAV-29578: Velger navn basert på master fra PDL

--- src/test/kotlin/no/nav/familie/klage/personopplysninger/pdl/PdlTestdata.kt
+++ src/test/kotlin/no/nav/familie/klage/personopplysninger/pdl/PdlTestdata.kt
@@ -4,7 +4,7 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 object PdlTestdata {
-    private val metadataGjeldende = Metadata(false)
+    private val metadataGjeldende = Metadata("PDL", false)
 
     private const val IDENT = "2"
 
